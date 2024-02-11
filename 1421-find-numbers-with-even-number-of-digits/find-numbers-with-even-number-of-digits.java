5c691e61352ada37f3d5c51638c7156b579d3e26
satyamkumarmishra2005 <142133113+satyamkumarmishra2005@users.noreply.github.com>
2024-02-11 19:09:41 +0530

    Time: 1 ms (98.59%) | Memory: 43.1 MB (28.99%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
@@ -1,16 +1,20 @@
 class Solution {
     public int findNumbers(int[] nums) {
-        int ans = 0;
-        for(int i = 0 ; i<nums.length;i++){
+      int ans = 0 ; 
+      for(int i = 0 ; i<nums.length;i++){
           int digit = 0;
           while(nums[i]>0){
-              nums[i]= nums[i]/10;
+              nums[i] = nums[i]/10;
               digit++;
           }
           if(digit%2==0){
               ans++;
-          }
-    }
-    return ans;
+          
+      }
+      }
+          // Another method for finding no of digits and to solve this question
+          //int digit = (int) Math.log10(nums[i]+1);
+          return ans;
+          
 }
 }
