388b04d380309e6e78aebaa18e36ef61679591ec
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2024-02-25 17:46:55 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (21.68%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,37 +1,35 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans = {-1,-1};
-        ans[0] = search(nums,target,true);
-        if(ans[0]!=-1){
-            ans[1] = search(nums,target,false);
-        }
-        return ans;
+ int[] ans = {-1,-1};
+  ans[0] = search(nums,target,true);
+  if(ans[0]!=-1){
+      ans[1] = search(nums,target,false);
+  }
+  return ans;
     }
-        
-    int search(int[]nums,int target,boolean firstindex){
-        int start = 0 ;
-        int end = nums.length-1;
-        int ans = -1;
-        while(start<= end){
-            int mid = start + (end-start)/2;
-            if(nums[mid]>target){
-                end = mid-1;
-            }
-            else if(nums[mid]<target){
-                start = mid + 1;
-            }
-            else{
-                ans = mid;
-
-                if(firstindex){
-                    end = mid-1;
+        int search(int[]nums,int target , boolean  FirstIndex){
+            int start = 0;
+            int end = nums.length-1;
+            int ans = -1;
+            while(start<=end){
+                int mid = start + (end-start)/2;
+                if(nums[mid]>target){
+                    end = mid - 1;
+                }
+                else if(nums[mid]<target){
+                    start = mid + 1;
                 }
                 else{
-                    start = mid+1;
+                ans = mid;
+                if (FirstIndex){
+                    end = mid - 1;
+                }
+                else{
+                    start = mid + 1;
+                }
                 }
             }
-        }
+        
         return ans;
     }
-   
-    }
+}
