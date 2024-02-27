9cdc76834c24d4f7f9225729aa5f501079f4b112
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2024-02-27 18:53:35 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (56.19%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,62 +1,32 @@
 class Solution {
     public int search(int[] nums, int target) {
-         int pivot = findPivot(nums);
+     int start = 0;
+     int end = nums.length-1;
 
-        // if you did not find a pivot, it means the array is not rotated
-        if (pivot == -1) {
-            // just do normal binary search
-            return binarySearch(nums, target, 0 , nums.length - 1);
-        }
+     while(start<=end){
+         int mid = start + (end-start)/2;
 
-        // if pivot is found, you have found 2 asc sorted arrays
-        if (nums[pivot] == target) {
-            return pivot;
-        }
-
-        if (target >= nums[0]) {
-            return binarySearch(nums, target, 0, pivot - 1);
-        }
-
-        return binarySearch(nums, target, pivot + 1, nums.length - 1);
-    }
-    
-     int binarySearch(int[] arr, int target, int start, int end) {
-        while(start <= end) {
-            // find the middle element
-//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
-            int mid = start + (end - start) / 2;
-
-            if (target < arr[mid]) {
-                end = mid - 1;
-            } else if (target > arr[mid]) {
-                start = mid + 1;
-            } else {
-                // ans found
-                return mid;
-            }
-        }
-        return -1;
-    }
-      static int findPivot(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            // 4 cases over here
-            if (mid < end && arr[mid] > arr[mid + 1]) {
-                return mid;
-            }
-            if (mid > start && arr[mid] < arr[mid - 1]) {
-                return mid-1;
-            }
-            if (arr[mid] <= arr[start]) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-        return -1;
+         if(nums[mid]==target){
+             return mid;
+         }
+         // left hand side is sorted serach in left hand side
+         if(nums[start]<=nums[mid]){
+             if(nums[start]<= target && target < nums[mid]){
+              end = mid - 1;
+             }
+             else{
+                 start = mid + 1;
+             }
+         }
+         else{
+             if(nums[mid]< target && target <= nums[end]){
+                 start = mid + 1;
+             }
+             else{
+                 end = mid -1;
+             }
+         }
+     }
+     return - 1;
     }
 }
-    
-    
