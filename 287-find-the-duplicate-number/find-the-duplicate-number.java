1b95dc1eb620510cb838020b4b9910edce25fa2a
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2024-03-10 20:18:49 +0530

    Time: 6 ms (55.50%) | Memory: 57.7 MB (77.75%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,38 +1,27 @@
 class Solution {
-    public int findDuplicate(int[] nums) {
-        // brute force approach 
-        // time complexity o(n*n)// as to loops are running 
-        // but it exceed the time limit
-        /*
-        int n = nums.length;
-        for(int i =0 ; i<n; i++){
-            for(int j = i+1; j<n ; j++){
-                if(nums[i]==nums[j]){
-                    return nums[i];
+    public int findDuplicate(int[] arr) {
+        int i = 0;
+        while(i<arr.length){
+            if(arr[i]!=i+1){
+                int correct = arr[i]-1;
+                if(arr[i]!=arr[correct]){
+                    swap(arr,i,correct);
                 }
+                else{
+                    return arr[i];
+                }
+
+            }
+            else{
+                i++;
             }
         }
         return -1;
-        */
-        int len = nums.length;
-        int start = 1;
-        int end = len-1;
-        while(start<end){
-            int mid = start + (end-start)/2;
-            int cnt = 0;
-            for(int i = 0 ; i<len; i++){
-            if(nums[i]<= mid){
-                cnt++;
-            }
-        }
-        if(cnt<=mid){
-            start = mid + 1;
-        }
-        else{
-            end = mid;
-        }
-    
     }
-  return start;  
-}
+
+    static void swap(int[]arr,int first , int second){
+        int temp = arr[first];
+        arr[first]= arr[second];
+        arr[second]= temp;
+    }
 }
