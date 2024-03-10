1d44b5508bb3eaa61a71ec501117e62a197b376e
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2024-03-10 18:49:16 +0530

    Time: 1 ms (39.18%) | Memory: 45 MB (69.95%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,31 +1,28 @@
 class Solution {
     public int missingNumber(int[] arr) {
+          int i = 0;
+          while(i<arr.length){
+              int correct = arr[i];
+              if(arr[i]<arr.length && arr[i]!=arr[correct] ){
+                  swap(arr,i,correct);
+              }
           
-        int i = 0;
-        while (i < arr.length) {
-            int correct = arr[i];
-            if (arr[i] < arr.length && arr[i] != arr[correct]) {
-                swap(arr, i , correct);
-            } else {
-                i++;
-            }
-        }
-
-        // search for first missing number
-        for (int index = 0; index < arr.length; index++) {
-            if (arr[index] != index) {
-                return index;
-            }
-        }
-
-        // case 2
-        return arr.length;
+          else{
+              i++;
+          }
+          }
+          // search the  first missingnumber
+          for(int index = 0 ; index <arr.length; index++){
+              if(arr[index]!= index){
+                  return index;
+              }
+          }
+          return arr.length;
     }
 
-    static void swap(int[] arr, int first, int second) {
+    static void swap(int [] arr , int first , int second){
         int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
+        arr[first]= arr[second];
+        arr[second]= temp;
     }
 }
-
