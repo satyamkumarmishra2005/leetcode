1714267883695854e1f13cd55bdc12bec53beb7c
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2024-10-12 10:36:12 +0530

    Time: 0 ms (100.00%) | Memory: 66.8 MB (64.75%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -1,22 +1,36 @@
 class Solution {
     public int findCenter(int[][] edges) {
         // Brute force
-        HashMap<Integer , Integer> freqMap = new HashMap<>();
-       int totaledges = edges.length;
-        for(int i = 0 ; i<edges.length ; i++){
-            int v1 = edges[i][0];
-            int v2 = edges[i][1];
+        //step1 create a freqMap which stores the frequency of vertices
+        // step2 return the vertex(node) whose freq is equal to total no of edges
 
-            freqMap.put(v1,freqMap.getOrDefault(v1,0)+1);
-            freqMap.put(v2,freqMap.getOrDefault(v2,0)+1);
+        // Time complexity = O(N) // edges
+    //     HashMap<Integer , Integer> freqMap = new HashMap<>();
+    //    int totaledges = edges.length;
+    //     for(int i = 0 ; i<edges.length ; i++){
+    //         int v1 = edges[i][0];
+    //         int v2 = edges[i][1];
 
-            if(freqMap.get(v1)==totaledges){
-                return v1;
-            }
-            if(freqMap.get(v2)==totaledges){
-                return v2;
-            }
-        }
-        return 0 ;
+    //         freqMap.put(v1,freqMap.getOrDefault(v1,0)+1);
+    //         freqMap.put(v2,freqMap.getOrDefault(v2,0)+1);
+
+    //         if(freqMap.get(v1)==totaledges){
+    //             return v1;
+    //         }
+    //         if(freqMap.get(v2)==totaledges){
+    //             return v2;
+    //         }
+    //     }
+    //     return 0 ;
+
+    // Optimal 
+
+    int [] pair1 = edges[0] ;// v1,v3
+    int [] pair2 = edges[1] ;// v3,v1
+
+    if(pair1[0]== pair2[0] || pair1[0]== pair2[1]){
+        return pair1[0];
     }
+    return pair1[1];
+}
 }
