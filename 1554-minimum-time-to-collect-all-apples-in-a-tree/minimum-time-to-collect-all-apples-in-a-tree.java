a37c28260a587e9bc3be38db61d98a2a2191c71f
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2024-10-15 18:46:37 +0530

    Time: 20 ms (77.27%) | Memory: 76.2 MB (63.48%) - LeetSync

---
+++ 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.java
@@ -0,0 +1,41 @@
+class Solution {
+
+      public int dfs(int src , int parent , List<List<Integer>> adjList ,List<Boolean> hasApple ){
+            int totalTime = 0;
+            for(int neighbour : adjList.get(src)){
+                if(neighbour == parent){
+                    continue;
+                }
+
+                int timetakenBychild = dfs(neighbour , src , adjList , hasApple);
+
+                if(timetakenBychild > 0 || hasApple.get(neighbour)){
+                    totalTime += timetakenBychild + 2;
+                }
+            }
+            return totalTime;
+
+        }
+    public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
+
+      
+        // Create an Adj list 
+        List<List<Integer>> adjList = new ArrayList<>();
+        for(int i =0 ; i< n ; i++){
+            adjList.add(new ArrayList<>());
+        }
+
+        for(int [] edge : edges){
+            int  from = edge[0];
+            int to = edge[1];
+            adjList.get(from).add(to);
+            adjList.get(to).add(from);
+
+
+
+
+        }
+
+        return dfs(0 , -1 , adjList , hasApple);
+    }
+}
