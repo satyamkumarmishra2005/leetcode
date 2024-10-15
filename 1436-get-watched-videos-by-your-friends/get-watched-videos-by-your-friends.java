634fc3e8b286ef01650b6a70e1e4ddb27555ae12
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2024-10-15 19:57:25 +0530

    Time: 24 ms (98.70%) | Memory: 46.3 MB (26.62%) - LeetSync

---
+++ 1436-get-watched-videos-by-your-friends/get-watched-videos-by-your-friends.java
@@ -0,0 +1,68 @@
+class Solution {
+    class pair implements Comparable<pair>{
+        String video ;
+        int freq;
+
+        pair(String video , int freq){
+            this.video = video;
+            this.freq = freq;
+        }
+
+        public int compareTo(pair that){
+            if(this.freq == that.freq){
+                return this.video.compareTo(that.video);
+            }
+           return  this.freq - that.freq;
+        }
+    }
+    public List<String> watchedVideosByFriends(List<List<String>> watchedVideos, int[][] friends, int id, int level) {
+        Queue<Integer> queue = new LinkedList<>();
+        HashSet<Integer> visited = new HashSet<>();
+
+        queue.offer(id);
+        visited.add(id);
+        int currentLevel = 0;
+      while(!queue.isEmpty()){
+        int size = queue.size();
+      
+
+    
+        // Iterate level by level
+        for(int i =0 ; i < size ; i++){
+           int  curId = queue.poll();
+
+           for(int friend : friends[curId]){
+            if(!visited.contains(friend)){
+                visited.add(friend);
+                queue.offer(friend);
+            }
+           }
+        }
+
+        currentLevel++;
+        if(currentLevel == level){
+            break;
+        }
+      }
+
+      HashMap<String , Integer> freqMap = new HashMap<>();
+      while(!queue.isEmpty()){
+        int curId = queue.poll();
+
+        for(String video : watchedVideos.get(curId) ){
+            freqMap.put(video , freqMap.getOrDefault(video,0)+1);
+        }
+      }
+      List<pair> videoList = new ArrayList<>();
+      for(String video : freqMap.keySet()){
+        videoList.add(new pair(video , freqMap.get(video)));
+      }
+      Collections.sort(videoList);
+      List<String> res = new ArrayList<>();
+      for(pair p : videoList){
+          res.add(p . video);
+      }
+      
+    return res;
+}
+}
