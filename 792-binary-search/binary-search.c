54fcae228c0e67e180cfbb5ce72a043f8ab3c3af
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2024-10-23 11:47:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (55.56%) - LeetSync

---
+++ 792-binary-search/binary-search.c
@@ -0,0 +1,21 @@
+int search(int nums[], int numsSize, int target) {
+     int start = 0;
+    int end = numsSize - 1;
+    
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+        
+        if (nums[mid] == target) {
+            return mid;
+        }
+        else if (nums[mid] > target) {
+            end = mid - 1;
+        }
+        else {
+            start = mid + 1;
+        }
+    }
+    
+    return -1;  // Target not found
+}
+
