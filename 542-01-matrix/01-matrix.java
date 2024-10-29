df3d4be870a9fe28cff4b418cac72b88f73751e7
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2024-10-29 11:07:34 +0530

    Time: 13 ms (87.78%) | Memory: 47.1 MB (57.53%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,44 +1,52 @@
 class Solution {
-
-    int n ;
     int m ;
-     int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    public int[][] updateMatrix(int[][] matrix) {
-     m = matrix.length;
-        n = matrix[0].length;
-        
-        int[][] result = new int[m][n];
-        
-        Queue<int[]> que = new LinkedList<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    result[i][j] = 0;
-                    que.offer(new int[] {i, j});
+    int n ;
+
+    int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    public int[][] updateMatrix(int[][] mat) {
+
+        m = mat.length;
+        n = mat[0].length;
+
+        Queue<int [] > queue = new LinkedList<>();
+        int [][] res = new int [m][n];
+
+        for(int i =0 ; i< m ; i++){
+            for(int j =0 ; j< n ; j++){
+                if(mat[i][j] == 0){
+                    res[i][j] =0;
+                    queue.offer(new int [] {i,j});
                 }
-                else {
-                    result[i][j] = -1;
+
+                else{
+                    res[i][j] = -1;
                 }
+
             }
         }
 
-        while (!que.isEmpty()) {
-            int[] cell = que.poll();
+        while(!queue.isEmpty()){
+            int [] cell = queue.poll();
+
             int i = cell[0];
             int j = cell[1];
-            
-            for (int[] dir : directions) {
-                
+
+            for(int[] dir : directions){
                 int new_i = i + dir[0];
                 int new_j = j + dir[1];
-                
-                if(new_i >= 0 && new_i < m && new_j >= 0 && new_j < n && result[new_i][new_j] == -1) {
-                    result[new_i][new_j] = result[i][j] + 1;
-                    que.add(new int[] {new_i, new_j});
+
+                if(new_i  >= 0 && new_i < m && new_j >=0 && new_j < n && res[new_i][new_j] == -1){
+                    res[new_i][new_j] = res[i][j] + 1;
+
+                    queue.add(new int [] {new_i , new_j});
                 }
             }
+
+
         }
+
+        return res;
         
-        return result;
     }
 }
