70bea4a49cdffd17c8bed1c0d67aa6b7dfa99e5e
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2024-11-02 13:14:15 +0530

    Time: 6 ms (74.14%) | Memory: 45 MB (66.21%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -2,7 +2,8 @@ class Solution {
     public boolean canFinish(int n, int[][] prerequisites) {
         List<Integer>[] adj = new List[n];
         int[] indegree = new int[n];
-        List<Integer> ans = new ArrayList<>();
+       // List<Integer> ans = new ArrayList<>();
+       int count = 0;
 
         for (int[] pair : prerequisites) {
             int course = pair[0];
@@ -17,24 +18,26 @@ class Solution {
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < n; i++) {
             if (indegree[i] == 0) {
+                count ++;
                 queue.offer(i);
             }
         }
 
         while (!queue.isEmpty()) {
             int node = queue.poll();
-            ans.add(node);
+         //   ans.add(node);
 
             if (adj[node] != null) {
                 for (int next : adj[node]) {
                     indegree[next]--;
                     if (indegree[next] == 0) {
+                        count ++;
                         queue.offer(next);
                     }
                 }
             }
         }
 
-        return ans.size() == n;
+        return count == n; // return true;
     }
 }
