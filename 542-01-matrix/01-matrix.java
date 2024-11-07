acb3cbc1d87e67b942b6aa9b66a52a3891df43d2
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2024-11-07 20:16:13 +0530

    Time: 14 ms (67.66%) | Memory: 47.4 MB (36.86%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -2,7 +2,9 @@ class Solution {
     int m ;
     int n ;
 
-    int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    // we are performing BFS from 0 to all 1
+
+    int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};  // Directions in which we can move {upward , Downward , left , right}
 
     public int[][] updateMatrix(int[][] mat) {
 
@@ -12,19 +14,23 @@ class Solution {
         Queue<int [] > queue = new LinkedList<>();
         int [][] res = new int [m][n];
 
+          // Initialize the queue with all 0 cells and mark 1 cells as unvisited (-1)
+
         for(int i =0 ; i< m ; i++){
             for(int j =0 ; j< n ; j++){
                 if(mat[i][j] == 0){
-                    res[i][j] =0;
-                    queue.offer(new int [] {i,j});
+                    res[i][j] =0;  // Distance of 0 cells from themselves is 0
+                    queue.offer(new int [] {i,j});  // Add all 0 cells to the queue
                 }
 
                 else{
-                    res[i][j] = -1;
+                    res[i][j] = -1;  // Mark all 1 cells as unvisited
                 }
 
             }
         }
+
+         // Perform BFS starting from all 0 cells
 
         while(!queue.isEmpty()){
             int [] cell = queue.poll();
@@ -36,8 +42,14 @@ class Solution {
                 int new_i = i + dir[0];
                 int new_j = j + dir[1];
 
+                //new_i = i + dir[0] = 1 + (-1) = 0 (moves up to row 0).
+              //  new_j = j + dir[1] = 1 + 0 = 1 (column stays the same).
+               // This gives (new_i, new_j) = (0, 1), which is the cell above (1, 1). 
+
+                  // Check bounds and if the neighbor has been visited
+
                 if(new_i  >= 0 && new_i < m && new_j >=0 && new_j < n && res[new_i][new_j] == -1){
-                    res[new_i][new_j] = res[i][j] + 1;
+                    res[new_i][new_j] = res[i][j] + 1;  // Set distance for the neighbor
 
                     queue.add(new int [] {new_i , new_j});
                 }
