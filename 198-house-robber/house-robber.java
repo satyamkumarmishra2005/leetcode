f83166ca3cd1a63d52744f1ea5dd29bfd78d4915
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2024-11-14 22:01:11 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (88.95%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -32,23 +32,48 @@ class Solution {
 
 
 // Tabulation
+// Time complexity = O(N)
+// spcae complexity =O(N)
 public int rob(int[] nums) {
-    int n = nums.length;
-    if (n == 0) return 0;
-    if (n == 1) return nums[0];
+//     int n = nums.length;
+//     if (n == 0) return 0;
+//     if (n == 1) return nums[0];
 
-    int[] dp = new int[n];
-    dp[0] = nums[0];
-    dp[1] = Math.max(nums[0], nums[1]);
+//     int[] dp = new int[n];
+//     dp[0] = nums[0];
+//     dp[1] = Math.max(nums[0], nums[1]);
 
-    for (int i = 2; i < n; i++) {
-        int pick = nums[i] + dp[i - 2];
-        int notPick = dp[i - 1];
-        dp[i] = Math.max(pick, notPick);
-    }
+//     for (int i = 2; i < n; i++) {
+//         int pick = nums[i] + dp[i - 2];
+//         int notPick = dp[i - 1];
+//         dp[i] = Math.max(pick, notPick);
+//     }
 
-    return dp[n - 1];
+//     return dp[n - 1];
+// }
+
+
+// space optimization
+int n = nums.length;
+int prev = nums[0];
+int prev2 = 0;
+
+for(int i =1 ; i<n ; i++ ){
+    
+    int pick = nums[i];
+
+    if(i>1)
+
+    pick+= prev2;
+
+    int nonpick = prev;
+
+    int curr_i = Math.max(pick , nonpick);
+    prev2 = prev;
+    prev = curr_i;
 }
 
+return prev;
 
+}
 }
