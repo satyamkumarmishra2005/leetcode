dfa6a1c2b954b13a41de5c13fa7c332dfe4d884c
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2024-11-14 12:17:53 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (39.77%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -33,25 +33,40 @@ class Solution {
 
 
     // using tabulation 
-    if(n== 0){
+    // if(n== 0){
+    //     return 0;
+    // }
+
+    // if(n==1){
+    //     return 1;
+    // }
+     
+    //  int [] dp = new int [n+1];
+
+    //    dp[0] = 0;
+    //    dp[1] = 1;
+
+    //    for(int i =2 ; i< n+1 ; i++){
+    //        dp[i] = dp[i-1] + dp[i-2];
+    //    }
+
+    //    return dp[n];
+
+
+
+    // Space optimization
+    if(n == 0){
         return 0;
     }
+     int prev = 1;
+        int prev2 = 0;
 
-    if(n==1){
-        return 1;
-    }
-     
-     int [] dp = new int [n+1];
-
-       dp[0] = 0;
-       dp[1] = 1;
-
-       for(int i =2 ; i< n+1 ; i++){
-           dp[i] = dp[i-1] + dp[i-2];
-       }
-
-       return dp[n];
-   
+        for(int i = 2 ; i<= n ; i++){
+            int curr = prev + prev2;
+            prev2 = prev ;
+            prev = curr;
+        }
+      return prev;
 
      }
 
