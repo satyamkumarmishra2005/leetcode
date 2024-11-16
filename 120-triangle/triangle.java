b0d7276c4692690ce09e4ebba648b434f269779b
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2024-11-16 14:06:39 +0530

    Time: 4 ms (53.50%) | Memory: 44.1 MB (48.69%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -47,6 +47,8 @@ class Solution {
 
     int [][] dp = new int [n][n];
 
+      // Initialize the bottom row of dp with the values from the bottom row of the triangle
+
     for(int j =0 ; j < n ; j++){
         dp[n-1][j] = triangle.get(n-1).get(j);
     };
@@ -55,14 +57,18 @@ class Solution {
     for(int i = n-2 ; i>=0 ; i--){
         for(int j = i ; j>=0 ; j--){
 
+
+ // Calculate the two possible paths: moving down or moving diagonally
     int down = triangle.get(i).get(j) + dp[i+1][j];
     int diagonal = triangle.get(i).get(j) + dp[i+1][j+1];
+
+     // Store the minimum of the two paths in dp
 
     dp[i][j] = Math.min(down , diagonal);
 
         }
     }
-
+   // The result is stored at the top of dp array
     return dp[0][0];
 
    }
