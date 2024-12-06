1a62ee72969638ebb0ff3ab79c69b0a566583568
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2024-12-06 12:39:29 +0530

    Time: 22 ms (46.80%) | Memory: 50.8 MB (48.12%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,35 +1,73 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
+    // public int longestCommonSubsequence(String text1, String text2) {
+    //     // Memoaization
+    //     int n = text1.length();
+    //     int m = text2.length();
+
+    //     int[][] dp = new int[n][m];
+
+    //     for(int [] rows : dp){
+    //         Arrays.fill(rows,-1);
+    //     }
+
+    //     return maxsubsequence(text1,text2 , n-1 ,m-1 , dp);
+
+        
+    // }
+
+    // static int maxsubsequence(String text1 , String text2 , int index1 , int index2 , int[][] dp){
+    //     if(index1 < 0 || index2 < 0){
+    //         return 0;
+    //     }
+
+    //     if(dp[index1][index2]!= -1){
+    //         return dp[index1][index2];
+    //     }
+
+
+    //     if (text1.charAt(index1) == text2.charAt(index2)){
+    //     return   dp[index1][index2] =  1 + maxsubsequence(text1 , text2 , index1-1 , index2-1 , dp);
+    //     }
+
+    //     else{
+    //         return dp[index1][index2] = Math.max(maxsubsequence(text1 , text2 , index1-1 , index2 , dp), maxsubsequence(text1 , text2 , index1 , index2-1 , dp));
+    //     }
+    // }
+
+    // Tabulation
+
+    public int longestCommonSubsequence(String text1, String text2){
         int n = text1.length();
         int m = text2.length();
 
-        int[][] dp = new int[n][m];
+        int [][] dp = new int[n+1][m+1];
 
         for(int [] rows : dp){
             Arrays.fill(rows,-1);
         }
 
-        return maxsubsequence(text1,text2 , n-1 ,m-1 , dp);
-
+        for(int i = 0 ; i<= n ; i++){
+            dp[i][0] = 0;
+        }
         
-    }
-
-    static int maxsubsequence(String text1 , String text2 , int index1 , int index2 , int[][] dp){
-        if(index1 < 0 || index2 < 0){
-            return 0;
+        for(int j =0 ; j<=m ; j++){
+            dp[0][j] = 0;
         }
 
-        if(dp[index1][index2]!= -1){
-            return dp[index1][index2];
+        for(int index1 = 1; index1 <= n ; index1++){
+            for(int index2 = 1; index2 <= m ; index2++){
+
+                if(text1.charAt(index1-1)== text2.charAt(index2-1)){
+                   dp[index1][index2] = 1 + dp[index1 - 1][index2 - 1];
+                }
+
+                else{
+                     dp[index1][index2] = Math.max(dp[index1 - 1][index2], dp[index1][index2 - 1]);
+                }
+            }
         }
 
+          return dp[n][m];
 
-        if (text1.charAt(index1) == text2.charAt(index2)){
-        return   dp[index1][index2] =  1 + maxsubsequence(text1 , text2 , index1-1 , index2-1 , dp);
-        }
-
-        else{
-            return dp[index1][index2] = Math.max(maxsubsequence(text1 , text2 , index1-1 , index2 , dp), maxsubsequence(text1 , text2 , index1 , index2-1 , dp));
-        }
     }
 }
