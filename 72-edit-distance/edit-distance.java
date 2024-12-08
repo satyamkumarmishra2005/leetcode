d1c880856c10fbb9ad1827dad8ac38f60c36fb86
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2024-12-08 12:10:59 +0530

    Time: 3 ms (96.84%) | Memory: 45.2 MB (16.68%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,33 +1,75 @@
 class Solution {
+    //Memoaization
+    // public int minDistance(String word1, String word2) {
+
+    //     int n = word1.length();
+    //     int m = word2.length();
+
+    //     int[][] dp = new int[n][m];
+
+    //     for(int[] rows : dp){
+    //         Arrays.fill(rows,-1);
+    //     }
+
+    //     return editdistance(word1,word2 , n-1 , m-1 , dp);
+        
+    // }
+
+    // static int editdistance(String word1 , String word2 , int i , int j , int[][]dp){
+    //     if(i<0){
+    //         return j+1;
+    //     }
+
+    //     if(j<0){
+    //         return i+1;
+    //     }
+
+    //     if(dp[i][j]!= -1){
+    //         return dp[i][j];
+    //     }
+
+    //     if(word1.charAt(i)==word2.charAt(j)){
+    //     return dp[i][j] = editdistance(word1 , word2 , i-1 , j-1 , dp);
+    //     }
+
+    //     else{
+    //         return dp[i][j] = 1 + Math.min(editdistance(word1, word2, i - 1, j - 1, dp),
+    //                 Math.min(editdistance(word1, word2, i - 1, j, dp), editdistance(word1, word2, i, j - 1, dp)));
+    //     }
+
+
+    // }
+
+// Tabulation
     public int minDistance(String word1, String word2) {
 
         int n = word1.length();
         int m = word2.length();
 
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n+1][m+1];
 
         for(int[] rows : dp){
             Arrays.fill(rows,-1);
         }
 
-        return editdistance(word1,word2 , n-1 , m-1 , dp);
+        return editdistance(word1,word2 , n , m , dp);
         
     }
 
     static int editdistance(String word1 , String word2 , int i , int j , int[][]dp){
-        if(i<0){
-            return j+1;
+        if(i==0){
+            return j;
         }
 
-        if(j<0){
-            return i+1;
+        if(j==0){
+            return i;
         }
 
         if(dp[i][j]!= -1){
             return dp[i][j];
         }
 
-        if(word1.charAt(i)==word2.charAt(j)){
+        if(word1.charAt(i-1)==word2.charAt(j-1)){
         return dp[i][j] = editdistance(word1 , word2 , i-1 , j-1 , dp);
         }
 
