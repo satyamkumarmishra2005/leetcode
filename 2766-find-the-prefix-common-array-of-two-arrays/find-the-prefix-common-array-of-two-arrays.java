b04e9dffb6d8063ee4a933cc9a87766d13586612
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2025-01-14 21:34:54 +0530

    Time: 5 ms (61.72%) | Memory: 45.8 MB (28.65%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
@@ -1,73 +1,29 @@
 class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        // optimized solution
+        int n = A.length;
 
-        // Brute Force 
-      //  Time complexity: O(n3)
-//         int n = A.length;
+        int result [] = new int [n];
 
-        
 
-//     int prefixcommonArray [] = new int [n];  // created a prefixcommonArray to store the count of common elements
+        HashMap<Integer ,Integer> map = new HashMap<>();
+        int count =0;
+
+        for(int i = 0 ; i< n ; i++){
+            map.put(A[i] , map.getOrDefault(A[i], 0) +1 );
+            if(map.get(A[i])==2){
+                count++;
+            }
+
+             map.put(B[i] , map.getOrDefault(B[i], 0) +1 );
+            if(map.get(B[i])==2){
+                count++;
+            }
+
+            result[i] = count ;
+        }
     
-//      // Loop through each index to calculate common elements for each prefix
-//     for(int currentindex = 0 ; currentindex< n ; currentindex++){
-//         int commoncount = 0;
-
-    
-//         for(int Aindex = 0 ; Aindex<= currentindex ; Aindex++){
-//             for(int Bindex = 0 ; Bindex<= currentindex ; Bindex++){
-
-//                 if(A[Aindex]==B[Bindex]){
-//                     commoncount ++;
-//                     break;   //Prevent counting Duplicates
-//                 }
-//             }
-//         }
-//       // Store the count of common elements for the current prefix
-//         prefixcommonArray[currentindex] = commoncount;
-//     }
-//      return prefixcommonArray;
-//     }
-// }
-
-// Optimized Approch using Hashset
-
-// Time complexity: O(n2)
-//space complexity : O(n)
-
-int n = A.length;
-
-int prefixcommonArray[] = new int [n];
-
-// Initialize sets to store elements from A and B
-
-Set<Integer> elementInA = new HashSet<Integer>();
-Set<Integer> elementInB = new HashSet<Integer>();
-
- // Iterate through the elements of both arrays
-
-for(int currentIndex = 0 ; currentIndex< n ; currentIndex++){
-
-      // Add current elements from A and B to respective sets
-  elementInA.add(A[currentIndex]);
-  elementInB.add(B[currentIndex]);
-
-   int commoncount = 0;
-   
-    // Count common elements between the sets
-
-   for(int element :  elementInA ){
-    if( elementInB.contains(element)){
-         commoncount++;
-    }
-   }
-   // Store the count of common elements for the current prefix
-   prefixcommonArray[currentIndex] =  commoncount;
-    
-}
-
-return  prefixcommonArray;
-
+    return result;
 
     }
 }
