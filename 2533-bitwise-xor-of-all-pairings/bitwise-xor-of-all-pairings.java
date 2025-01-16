3946cbab499424b46aec6f142dec0d4dbfecf715
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2025-01-16 09:18:53 +0530

    Time: 44 ms (6.90%) | Memory: 62.9 MB (5.17%) - LeetSync

---
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.java
@@ -0,0 +1,33 @@
+class Solution {
+   // Time complexity = O(N)
+   // Space complexity = O(N)
+
+   // Here first we will create a Map which stores the frequency of the nums1 and nums2 array
+   // if the frequency of number is 2 then the Xor of that number will be Zero because the Xor of same elemnts is zero
+   // hence of the frequency of any element is odd then the answer will be Xor of those element we will store them in result 
+   // and return the result
+    public int xorAllNums(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        for(int num : nums1){
+            mp.put(num , mp.getOrDefault(num , 0) + n);
+        }
+
+
+        for(int num : nums2){
+            mp.put(num , mp.getOrDefault(num,0) + m);
+        }
+       
+       int result = 0;
+
+       for(int num : mp.keySet()){
+        if(mp.get(num)%2 != 0){
+            result ^= num ;
+        }
+       }
+        return result;
+    }
+}
