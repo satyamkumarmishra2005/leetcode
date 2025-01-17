f12bd1d590b016a7c7143d9788dc77a7a9e3a5e1
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2025-01-17 11:03:23 +0530

    Time: 2 ms (100.00%) | Memory: 62.4 MB (40.59%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
@@ -1,33 +1,11 @@
 class Solution {
     public boolean doesValidArrayExist(int[] derived) {
-        int n = derived.length;
+        int xor = 0;
 
-        int [] original = new int [n];
-
-        // Take original[0] = 0;
-
-        original[0] = 0;
-
-        for(int i = 0 ; i< n-1 ; i++){
-            original[i+1] = original[i] ^ derived[i];
+        for(int x : derived){
+            xor =( xor ^ x );
         }
 
-        if((original [n-1] ^ original[0]) == derived[n-1]){
-            return true;
-        }
-
-        // Lets try with original[0] = 1;
-
-        original[0] = 1;
-
-        for(int i = 0 ; i< n-1 ; i++){
-            original[i+1] = original[i] ^ derived[i];
-        }
-
-        if((original [n-1] ^ original[0]) == derived[n-1]){
-            return true;
-        }
-
-        return false;
+        return xor == 0; // if xor of derived elemnets is 0 then it is valid 
     }
 }
