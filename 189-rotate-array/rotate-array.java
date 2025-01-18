e973786f2ad87a71c8f04c6354734d8aaa9f5507
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2025-01-18 12:11:52 +0530

    Time: 0 ms (100.00%) | Memory: 57.5 MB (47.85%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,14 +1,23 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int n =  nums.length;
-        int [] res = new int [n];
-        
-        for(int i = 0 ; i< n ; i++){
-          res[(i + k ) % n ] = nums[i];
-        }
-        
-        for(int i =0 ; i< n ; i++){
-            nums[i] = res[i];
+        // optimised solution
+        // Time complexity = O(N)
+        // Spcae complexity = O(1)
+        int n = nums.length;  // The number of rotations k is updated to k % n to handle cases 
+        //where k is greater than n. This ensures that rotating the array n times results in the same array, so any extra rotations beyond n can be ignored.
+         k = k % n ;
+        reverse(nums,0 , n-1);
+        reverse(nums , 0 , k-1);
+        reverse(nums , k , n-1);
+    }
+
+    public void reverse(int [] nums,int start , int end){
+        while(start< end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start ++ ;
+            end --;
         }
     }
 }
