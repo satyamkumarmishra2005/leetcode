65322d5960bcb3ed76793670d6a6159365e629b2
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2025-01-26 11:56:18 +0530

    Time: 6 ms (78.75%) | Memory: 49.3 MB (56.79%) - LeetSync

---
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxTurbulenceSize(int[] A) {
+        int inc = 1, dec = 1, result = 1;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] < A[i - 1]) {
+                dec = inc + 1;
+                inc = 1;
+            } else if (A[i] > A[i - 1]) {
+                inc = dec + 1;
+                dec = 1;
+            } else {
+                inc = 1;
+                dec = 1;
+            }
+            result = Math.max(result, Math.max(dec, inc));
+        }
+        return result;
+    }
+}
