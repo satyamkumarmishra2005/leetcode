60944ee650a9477d114e5989787c858901bdaf81
Satyam Mishra <142133113+satyamkumarmishra2005@users.noreply.github.com>
2025-01-29 19:10:55 +0530

    Time: 24 ms (73.73%) | Memory: 47.1 MB (15.83%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,17 +1,25 @@
 class Solution {
-    public int subarraySum(int[] arr, int k) {
-         HashMap<Integer , Integer> map = new HashMap<>();
-        //(sum,count)
-        map.put(0,1); // edge case if the sum of entire array is equal to k
-        int sum =0 ;
-        int ans = 0 ;
-        for(int j =0 ; j<arr.length ; j++){
-            sum+= arr[j];
+    public int subarraySum(int[] nums, int k) {
+        //Optimal
+        //(sum , count)
+        int n = nums.length;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0,1); // for the test case if the value of k=0
+
+        int sum =0;
+        int ans = 0;
+
+        for(int j =0 ; j<n ; j++){
+            sum+= nums[j];
             if(map.containsKey(sum-k)){
                 ans+= map.get(sum-k);
             }
-            map.put(sum,map.getOrDefault(sum,0)+1);
+
+            map.put(sum, map.getOrDefault(sum , 0)+1);
         }
-        return ans;
+     return ans;
+
     }
 }
