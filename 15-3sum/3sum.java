a0e7d716de3bccdbaeabde26fe0e022c052b93fe
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2025-06-22 11:04:46 +0530

    Time: 30 ms (69.43%) | Memory: 51.3 MB (83.15%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,34 +1,71 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        if(nums== null && nums.length < 3){
-            return new ArrayList<>();
-        }
-        Set<List<Integer>> res = new HashSet<>();
-        int n = nums.length;
+         //Better   Used hashSet
+        // if(nums== null && nums.length < 3){
+        //     return new ArrayList<>();
+        // }
+        // Set<List<Integer>> res = new HashSet<>(); // we have used hashset here to avoid duplication
+        // int n = nums.length;
+        // Arrays.sort(nums);
+
+        // for(int i =0 ; i< n-2 ; i++){
+        //     int j = i+1;
+        //     int k = n-1;
+
+        //     while(j<k){
+        //         int sum = nums[i]+ nums[j] + nums[k];
+
+        //         if(sum==0){
+        //          res.add(Arrays.asList(nums[i], nums[j], nums[k]));   
+        //          j++;
+        //          k--;
+        //             }
+
+        //         else if(sum<0){
+        //             j++;
+        //         }
+        //         else{
+        //             k--;
+        //         }
+        //     }
+        // }
+        // return new ArrayList<>(res);
+
+
+    // More optimal Without Hahset
+  List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 3) return res;
+
         Arrays.sort(nums);
+        int n = nums.length;
 
-        for(int i =0 ; i< n-2 ; i++){
-            int j = i+1;
-            int k = n-1;
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicates for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            while(j<k){
-                int sum = nums[i]+ nums[j] + nums[k];
+            int j = i + 1, k = n - 1;
 
-                if(sum==0){
-                 res.add(Arrays.asList(nums[i], nums[j], nums[k]));   
-                 j++;
-                 k--;
-                    }
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
 
-                else if(sum<0){
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     j++;
-                }
-                else{
+                    k--;
+
+                    // Skip duplicates for j
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    // Skip duplicates for k
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+
+                } else if (sum < 0) {
+                    j++;
+                } else {
                     k--;
                 }
             }
         }
-        return new ArrayList<>(res);
-        
+
+        return res;
     }
 }
