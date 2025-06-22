6347af03d9debe860cecebc1a1a44444d96279d4
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2025-06-22 10:24:48 +0530

    Time: 830 ms (14.61%) | Memory: 52.5 MB (22.31%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,43 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-  
-        int n = nums.length ;
-     Arrays.sort(nums);// Sort the input array nums in non-decreasing order. Sorting the array helps in avoiding duplicate triplets.
+        if(nums== null && nums.length < 3){
+            return new ArrayList<>();
+        }
+        Set<List<Integer>> res = new HashSet<>();
+        int n = nums.length;
+        Arrays.sort(nums);
 
-        for(int i = 0 ; i< nums.length ; i++){
-            if(i> 0  && nums[i]== nums[i-1]){  // Check if the current element is a duplicate of the previous element and skip it if it is.
-                continue;  
-            }
-
-            int j = i+1; // Use a two-pointer approach with pointers j and k to find triplets whose sum equals zero.
+        for(int i =0 ; i< n-2 ; i++){
+            int j = i+1;
             int k = n-1;
 
             while(j<k){
-                int total = nums[i] + nums[j] + nums[k];  // Calculate the total sum of the current triplet.
+                int sum = nums[i]+ nums[j] + nums[k];
 
-                if(total > 0){   //If the total sum is greater than zero, decrement the k pointer to decrease the total sum.
-                    k--;
-                }
+                if(sum==0){
+                 res.add(Arrays.asList(nums[i], nums[j], nums[k]));   
+                 j++;
+                 k--;
+                    }
 
-               else if(total < 0){   // If the total sum equals zero, add the triplet [nums[i], nums[j], nums[k]] to the result list res.
+                else if(sum<0){
                     j++;
                 }
-
                 else{
-                    res.add(Arrays.asList(nums[i], nums[j] , nums[k]));  // If the total sum equals zero, add the triplet [nums[i], nums[j], nums[k]] to the result list res.
-                    j++;     // Increment the j pointer to skip any duplicate elements
-
-                    while(nums[j]== nums[j-1] && j< k){
-                        j++;
-                    }
+                    k--;
                 }
-
-
             }
         }
-
-        return res; // Return the list res containing all the unique triplets whose sum is zero.
-
+        return new ArrayList<>(res);
+        
     }
 }
