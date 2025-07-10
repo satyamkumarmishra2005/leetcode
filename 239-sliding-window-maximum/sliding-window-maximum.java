b3e1a226210e828a23f40e98adc4caa5bada931a
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2025-07-10 11:10:49 +0530

    Time: 28 ms (92.00%) | Memory: 59.7 MB (38.89%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,48 +1,49 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
 
-         int ans[] = new int[nums.length - k + 1];
+        int ans[] = new int[nums.length-k+1];
 
         Deque<Integer> dq = new LinkedList<>();
 
-        int i =0;
-        int j=0;
+
         int n = nums.length;
 
-        while(j< n){
-        if(dq.size()==0){
-            dq.add(nums[i]);
-        }
+        int i =0;
+        int j =0;
 
-        else{
-            while(dq.size()>0 && dq.peekLast()< nums[j]){
-                dq.removeLast();
+        while(j<n){
+
+            if(dq.size()==0){  // If the dq is empty then add the elements of the array nums
+                dq.add(nums[i]);
             }
 
-            dq.add(nums[j]);
+            else{
+                while(dq.size()>0 && dq.peekLast()< nums[j]){  // If the elemnts present in the dequeare smaller then the current element then remove those elements from the deque
+                    dq.removeLast();
+                }
+
+                dq.add(nums[j]);// add the largest element in the dequeue
+            }
+
+
+            if(j-i+1<k){
+                j++;
+            }
+            
+            else if(j-i+1==k){    
+                ans[i] = dq.peek(); // add the max element in the ans array
+
+                if(nums[i]==dq.peek()){  // if the current element and the elemnt present tin the front is same then remove the elemnt for sliding the window
+                    dq.removeFirst();
+                }
+
+                i++;
+                j++;
+            }
+
         }
 
-       
-       if(j-i+1<k){
-        j++;
-       }
-
-       else if(j-i+1==k){
-        ans[i] = dq.peek();
-        //Silde the window
-        // calculation
-
-        if(nums[i]==dq.peek()){
-            dq.removeFirst();
-        }
-
-        i++;
-        j++;
-       }
-
+        return ans;
         
     }
-
-    return ans;
-}
 }
