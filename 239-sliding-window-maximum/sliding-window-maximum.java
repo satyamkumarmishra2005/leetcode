05d1a04caa60ed41af579f81bac1bec90e784c7b
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2025-07-15 09:53:03 +0530

    Time: 29 ms (86.49%) | Memory: 59.9 MB (35.07%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,42 +1,47 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
 
-        Deque<Integer> dq = new LinkedList<>();
-        int [] ans = new int[nums.length-k+1];
+        int ans[] = new int[nums.length-k+1];
 
-        int i =0;
-        int j=0;
+        Deque<Integer> dq = new LinkedList<>();
+
 
         int n = nums.length;
 
+        int i =0;
+        int j =0;
+
         while(j<n){
-            if(dq.size()==0){
-              dq.add(nums[i]);
+
+            if(dq.size()==0){  // If the dq is empty then add the elements of the array nums
+                dq.add(nums[i]);
             }
 
             else{
-                while(dq.size()>0 && dq.peekLast()< nums[j]){
+                while(dq.size()>0 && dq.peekLast()< nums[j]){  // If the elemnts present in the dequeare smaller then the current element then remove those elements from the deque
                     dq.removeLast();
                 }
 
-                dq.add(nums[j]);
+                dq.add(nums[j]);// add the largest element in the dequeue
             }
 
-            if(j-i+1 <k){
+
+            if(j-i+1<k){
                 j++;
             }
+            
+            else if(j-i+1==k){    
+                ans[i] = dq.peek(); // add the max element in the ans array
 
-            else if(j-i+1==k){
-                ans[i] = dq.peek();
-
-                if(nums[i]== dq.peek()){
+                if(nums[i]==dq.peek()){  // if the current element and the elemnt present tin the front is same then remove the elemnt for sliding the window
                     dq.removeFirst();
                 }
+
                 i++;
                 j++;
             }
-        }
 
+        }
 
         return ans;
         
