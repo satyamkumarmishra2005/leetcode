37c8dce35ace31a262e746bbd093789c21731ced
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2025-08-01 18:52:18 +0530

    Time: 1 ms (34.70%) | Memory: 44.9 MB (97.21%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,15 +1,38 @@
 class Solution {
     public int missingNumber(int[] nums) {
+
+
+    // cyclic sort 
+    // Time complexity: O(N)
+    // space complexity : O(1)
+        int i = 0;
         int n = nums.length;
 
-        int totalsum = n * (n+1) / 2;
+        while(i< n){
+            int correct = nums[i];
 
-        int arraysum = 0;
-
-        for(int num : nums){
-            arraysum+= num ;
+            if(nums[i]< n && nums[i]!= nums[correct]){
+                swap(nums , i , correct);
+            }
+            else{
+                i++;
+            }
         }
 
-        return totalsum - arraysum ;
+        for(int x =0 ; x< n ; x++){
+            if(nums[x]!= x){
+                return x;
+            }
+        }
+
+
+        return n ;
+        
+    }
+
+    static void swap(int[] nums , int first , int second){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
     }
 }
