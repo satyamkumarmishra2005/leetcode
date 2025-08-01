df9892a6fbf4010684f57367d9713a5610b320b0
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2025-08-01 19:26:54 +0530

    Time: 6 ms (82.55%) | Memory: 57 MB (61.76%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,26 +1,30 @@
 class Solution {
-    public List<Integer> findDuplicates(int[] arr) {
-         int i = 0;
-        while (i < arr.length) {
-            int correct = arr[i] - 1;
-            if (arr[i] != arr[correct]) {
-                swap(arr, i , correct);
-            } else {
+    public List<Integer> findDuplicates(int[] nums) {
+        int i =0;
+
+     while(i<nums.length){
+            int correct = nums[i]-1;
+            if(nums[i]!= nums[correct]){
+                swap(nums,i,correct);
+            }
+            else{
                 i++;
             }
         }
 
-        List<Integer> ans = new ArrayList<>();
-        for (int index = 0; index < arr.length; index++) {
-            if (arr[index] != index+1) {
-                ans.add(arr[index]);
-            }
-        }
+         List<Integer> ans = new ArrayList<>();
 
+        for(int index = 0 ; index< nums.length ; index++){
+           
+            if(nums[index]!= index+1){
+                ans.add(nums[index]);
+            }
+
+        }
         return ans;
     }
 
-    static void swap(int[] arr, int first, int second) {
+    static void  swap(int[]arr, int first , int second){
         int temp = arr[first];
         arr[first] = arr[second];
         arr[second] = temp;
