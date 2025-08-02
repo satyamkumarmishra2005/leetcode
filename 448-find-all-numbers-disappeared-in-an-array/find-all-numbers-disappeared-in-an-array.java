35e033ceaf4838626e1c29a3d27360174fd7b00e
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2025-08-02 18:49:56 +0530

    Time: 6 ms (86.09%) | Memory: 56.8 MB (45.08%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,29 +1,37 @@
 class Solution {
-  public List<Integer> findDisappearedNumbers(int[] nums) {
-        int i = 0;
-        while (i < nums.length) {
-            int correct = nums[i] - 1;
-            if (nums[i] != nums[correct]) {
-                swap(nums, i , correct);
-            } else {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int i =0;
+        int n = nums.length;
+
+        while(i< nums.length){
+            int correct = nums[i]-1;
+
+            if(nums[i]!= nums[correct]){
+                swap(nums,correct, i);
+            }
+
+            else{
                 i++;
             }
         }
 
-        // just find missing numbers
+
         List<Integer> ans = new ArrayList<>();
-        for (int index = 0; index < nums.length; index++) {
-            if (nums[index] != index+1) {
-                ans.add(index + 1);
+        for(int index =0 ; index< nums.length; index++){
+            if(nums[index]!= index+1){
+                ans.add(index+1);
             }
+
+            
         }
 
         return ans;
+        
     }
 
-    static void swap(int[] arr, int first, int second) {
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
+    static void swap(int[] nums , int first , int second){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
     }
 }
