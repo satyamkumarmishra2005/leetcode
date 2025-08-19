971b7750c2caca981db3ea3fc49eb718f07ee680
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2025-08-19 19:26:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (29.16%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -15,34 +15,28 @@ class Solution {
             return head;
         }
 
-    
         ListNode dummy = new ListNode(0);
         dummy.next = head;
 
         ListNode prev = dummy;
 
-      // Step 1: move prev to the node before 'left'
-        for(int i = 1 ; i< left ; i++){
-            prev= prev.next;
+        for(int i =1 ; i< left ; i++){
+            prev = prev.next;
         }
+       
+       ListNode curr = prev.next;
 
-
-        ListNode curr = prev.next;
-
-        for(int i = 1 ; i<= right-left ; i++){
-
+        for(int i = 1; i<= right-left ; i++){
             ListNode temp = prev.next;
 
             prev.next = curr.next;
-
-            
-
             curr.next = curr.next.next;
 
             prev.next.next = temp;
+            
 
         }
-        
+
 
         return dummy.next;
         
