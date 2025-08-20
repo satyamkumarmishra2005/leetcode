f4518c0b66e27c2459e32c76dd65c1f9c7d811cc
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2025-08-20 19:27:53 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (5.45%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
@@ -1,14 +1,24 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
  * }
  */
+
+
+//  \U0001f914 Intuition
+// We know that previous node pointer to given node. What if we change given node value to next nodes and point next to next.next. It's essentially changing current node to next and deleting next.
+
+// \U0001f9e0 Approach
+// To delete the given node, we copy the value of the next node to the current node and adjust the next pointer to skip the next node.
+
+// Copy the value of the next node to the current node.
+// Adjust the next pointer to skip the next node.
 class Solution {
     public void deleteNode(ListNode node) {
-        node.val = node.next.val;
-        node.next = node.next.next;
+      node.val = node.next.val;
+      node.next= node.next.next;
     }
 }
