87ba856fbf9fc140cdc6956db6c3402967eb9811
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2025-08-29 10:35:52 +0530

    Time: 1 ms (21.90%) | Memory: 50.4 MB (5.65%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -14,18 +14,40 @@ class Solution {
 //     }
 // }
 
- public void reverseString(char[] s) {
+// Time complexity O(N)
+// Space complexity O(1)
+//  public void reverseString(char[] s) {
 
-    int left = 0 ; 
-    int right = s.length-1;
+//     int left = 0 ; 
+//     int right = s.length-1;
 
-    while(left<=right){
-     char temp = s[left];
-     s[left] = s[right];
-     s[right] = temp;
+//     while(left<=right){
+//      char temp = s[left];
+//      s[left] = s[right];
+//      s[right] = temp;
     
-    left++;
-    right--;
- }
+//     left++;
+//     right--;
+    
+//  }
+//}
+
+public void reverseString(char[] s) {
+    int n = s.length;
+    
+    reverse(s , 0 , n);
+}
+
+static void reverse(char[] s , int i , int n){
+    if(i>= n/2){
+        return ;
+    }
+
+    char temp = s[i];
+    s[i] = s[n-i-1];
+    s[n-i-1] = temp;
+
+
+    reverse(s, i+1 , n);
 }
 }
