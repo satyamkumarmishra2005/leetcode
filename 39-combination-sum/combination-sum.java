29ef10bd8feccc39603f61cf3aecfbb17d914e19
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2025-09-08 19:25:02 +0530

    Time: 2 ms (83.78%) | Memory: 44.7 MB (64.84%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -8,23 +8,38 @@ class Solution {
     }
 
     public void backtrack(int [] candidates , int target , int start , List<Integer> current , List<List<Integer>> result){
+    if(start == candidates.length){
+
+    
         if(target==0){
             result.add(new ArrayList<>(current));
+
+        }
             return ;
         }
 
-        for(int i = start ; i< candidates.length; i++){
-            int num = candidates[i];
+        // for(int i = start ; i< candidates.length; i++){
+        //     int num = candidates[i];
 
-            if(num > target){
-                continue;
-            }
+        //     if(num > target){
+        //         continue;
+        //     }
 
-            current.add(num);
+        //     current.add(num);
 
-            backtrack(candidates , target-num , i , current , result);
+        //     backtrack(candidates , target-num , i , current , result);
 
+        //     current.remove(current.size()-1);
+        // }
+
+        if(target>= candidates[start]){
+            current.add(candidates[start]);
+            backtrack(candidates , target- candidates[start] , start , current , result);
             current.remove(current.size()-1);
+
         }
+
+        backtrack(candidates ,  target , start+1 , current , result);
+
     }
 }
