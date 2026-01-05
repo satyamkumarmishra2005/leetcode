98abdb78b9b4e066962880f148a998d16eb8a72a
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-01-05 19:58:57 +0530

    Time: 1 ms (58.73%) | Memory: 48.9 MB (9.41%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,40 +1,47 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length ;
-        if(n== 0 || n==1){
-            return 0;
+
+        int n = height.length;
+
+        if(n==0 || n==1){
+            return 0 ;
         }
-         int [] leftmax = getLeftMax(height , n);
-        int [] rightmax = getRightMax(height , n);
-     int sum = 0;
-        for(int i = 0 ; i<n ; i++){
-    sum+= Math.min(leftmax[i] , rightmax[i])-height[i];
-        }
+
+        int [] leftmax = getleftmax(height, n);
+        int [] rightmax = getrightmax(height, n);
+
+      int sum = 0;
+
+      for(int i =0 ; i< n ; i++){
+        sum+= Math.min(leftmax[i], rightmax[i]) - height[i];
+      }
         return sum ;
+    
         
     }
 
-        public int [] getLeftMax(int[] height , int n){
-           int [] leftmax = new int[n];
-           leftmax[0] = height[0];
-           for(int i = 1; i< n ; i++){
-               leftmax[i] = Math.max(leftmax[i - 1], height[i]);
-           } 
-           return leftmax;
+    public int [] getleftmax(int[] height , int n){
+
+        int [] leftmax = new int[n];
+        leftmax[0] = height[0];
+
+        for(int i = 1 ; i< n ; i++){
+            leftmax[i]= Math.max(leftmax[i-1], height[i]);
         }
 
-        public int [] getRightMax(int [] height , int n){
-            int [] rightmax = new int [n];
-            rightmax[n-1] = height[n-1];
-
-            for(int i = n-2 ; i>=0 ; i--){
-                rightmax[i] = Math.max(rightmax[i+1], height[i])  ;  
-                        }
-
-                         return rightmax;
-        }
-       
+        return leftmax;
     }
 
-       
+    public int[] getrightmax(int[] height , int n){
+        int [] rightmax = new int[n];
+        rightmax[n-1] = height[n-1];
+
+        for(int i = n-2 ; i>=0 ; i--){
+            rightmax[i] = Math.max(rightmax[i+1], height[i]);
+        }
+
+        return rightmax;
+    }
+
     
+}
