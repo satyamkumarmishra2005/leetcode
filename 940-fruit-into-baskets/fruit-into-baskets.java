a95c02522de8d19fe2f8fa418a1825c0ced919a5
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-02-03 12:11:09 +0530

    Time: 54 ms (64.35%) | Memory: 70.9 MB (18.99%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,40 +1,40 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-
-
-        // Same question as Pick Toys and as Maximum subaaray with max K unique characters here k = 2
-
         int i =0;
-        int j =0;
-        int k =2 ;
-        
+        int j = 0;
+
+        int n = fruits.length;
+
+        int k =2;
+
         int max = 0;
 
-         int n = fruits.length;
-         HashMap<Integer,Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+      
 
-         while(j< n){
-        map.put(fruits[j], map.getOrDefault(fruits[j],0)+1);
+      while(j<n){
+        map.put(fruits[j], map.getOrDefault(fruits[j],0)+1);  // we are putting elements first till size k
+        
 
-        while(map.size()>k){
+        while(map.size()>k){   // if mapsize becomes greater than k then we will remove the left occurence minimize its size
             int left = fruits[i];
-
             map.put(left , map.get(left)-1);
 
             if(map.get(left)==0){
                 map.remove(left);
-               
             }
 
             i++;
+
         }
 
-        max = Math.max(max , j-i+1); // for condition when map.size()<=k
+        max = Math.max(max , j-i+1);    // when map.size() <=k ;
+
         j++;
 
-         }
 
-         return max;
-        
+      }
+
+        return max;
     }
 }
