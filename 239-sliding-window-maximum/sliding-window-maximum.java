863a4ef04db856783d0818dae7c7b588d172f7c3
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-02-06 10:39:19 +0530

    Time: 28 ms (90.57%) | Memory: 150 MB (33.01%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,40 +1,37 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int[] ans = new int[nums.length - k +1];
+
+        int [] ans = new int[nums.length-k+1];
 
         int i =0;
         int j =0;
 
+
         Deque<Integer> dq = new LinkedList<>();
+     
+      while(j< nums.length){
 
-        while(j< nums.length){
-            if(dq.size()==0){
-                dq.add(nums[i]);
-            }
-
-            else{
-                while(dq.size()>0 && dq.peekLast() < nums[j] ){
-                    dq.removeLast();
-                }
-
-                dq.add(nums[j]);
-            }
-
-            if(j-i+1 < k){
-                j++;
-            }
-
-            else if (j-i+1 == k){
-                ans[i] = dq.peek();
-
-                if(nums[i]== dq.peek()){
-                    dq.removeFirst();
-                }
-                i++;
-                j++;
-            }
+        while(!dq.isEmpty() && dq.peekLast()< nums[j]){
+            dq.removeLast();
         }
-    return ans;
 
+        dq.add(nums[j]);
+
+        if(j-i+1 < k){
+            j++;
+        }
+        else{
+            ans[i] = dq.peekFirst();
+
+            if(nums[i]== dq.peekFirst()){  // while sliding the window  remove the max element 
+                dq.removeFirst();
+            }
+            i++;  // here we are moving both i and j together so the window size remains constant always k 
+            j++;
+        }
+      }
+
+      return ans;
+        
     }
 }
