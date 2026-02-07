0a0513600718680bfeb78a192f90738223a76145
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-02-07 11:01:21 +0530

    Time: 6 ms (78.76%) | Memory: 51.2 MB (37.81%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
@@ -1,26 +1,32 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-        //The algorithm efficiently handles both normal and circular subarrays using Kadane's algorithm for maxsum and minsum.
-        //By subtracting the minimum subarray from the total sum, it calculates the sum of the circular subarray.
-        //Handles all-negative arrays by directly returning maxsum.
+
         int maxsum = nums[0];
         int minsum = nums[0];
+
         int currmax = 0;
         int currmin = 0;
+
         int totalsum = 0;
 
         for(int num : nums){
-            currmax= Math.max(currmax + num , num);
-            maxsum = Math.max(maxsum , currmax);
+
+            currmax = Math.max(currmax+ num , num);
             currmin = Math.min(currmin + num , num);
-            minsum = Math.min(minsum, currmin);
-            totalsum+= num ;
-            
+
+            maxsum= Math.max(currmax, maxsum);
+            minsum = Math.min(currmin,minsum);
+
+
+            totalsum = totalsum + num;
+
         }
-    
-        if(maxsum > 0){
-            return Math.max(maxsum , totalsum-minsum ); // cirrsum = totalsum- minsum;
+
+        if(maxsum>0){
+            return Math.max(maxsum , totalsum-minsum);
         }
-       return maxsum;
+
+       return  maxsum;
+        
     }
 }
