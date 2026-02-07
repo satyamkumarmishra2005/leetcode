d0d27a025a9bda5c6cfdf7d4af7640392e36a5bf
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-02-07 11:34:16 +0530

    Time: 7 ms (55.96%) | Memory: 67.1 MB (12.09%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
@@ -3,7 +3,9 @@ class Solution {
         int currmax = nums[0];
         int currmin = nums[0];
 
-        int maxsum = Math.abs(nums[0]);
+        int maxsum = Math.abs(nums[0]); // we are not taking maxsum = nums[0] because if the first elemnt is itself negative then it gedt returned 
+        // testcase  
+        //Editorial Input nums = [-1] Use Testcase Output -1 Expected 1
 
         for(int i= 1 ; i< nums.length ; i++){
             currmax = Math.max(currmax + nums[i], nums[i]);
@@ -14,5 +16,6 @@ class Solution {
 
         return maxsum;
         
+
     }
 }
