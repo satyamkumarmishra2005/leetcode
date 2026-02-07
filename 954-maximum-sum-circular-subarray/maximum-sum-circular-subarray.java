40dddd60911d0990d8b068d63428208aabc5d657
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-02-07 11:03:03 +0530

    Time: 6 ms (78.76%) | Memory: 51.2 MB (57.38%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
@@ -4,7 +4,7 @@ class Solution {
         int maxsum = nums[0];
         int minsum = nums[0];
 
-        int currmax = 0;
+        int currmax = 0;  // we are taking currmax as 0 because this array is also circular so we cannot just take currmax as nums[0];
         int currmin = 0;
 
         int totalsum = 0;
