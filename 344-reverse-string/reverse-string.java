fe72efb4a3a7365f55c62c01bf07468546fca2f4
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-02-09 18:22:45 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (52.32%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,53 +1,20 @@
 class Solution {
-//     public void reverseString(char[] s) {
-//        reverse(s,0,s.length-1);
-//     }
-//     private void reverse(char[] s , int left , int right){
-//         if(left >=  right){
-//             return;
-//         }
-//  char temp = s[left];
-//  s[left] = s[right];
-//  s[right] = temp;
+    public void reverseString(char[] s) {
 
-//  reverse(s,left+1 , right-1);
-//     }
-// }
+        int i =0;
+        int j = s.length-1;
 
-// Time complexity O(N)
-// Space complexity O(1)
-//  public void reverseString(char[] s) {
+        while(i<=j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
 
-//     int left = 0 ; 
-//     int right = s.length-1;
+            i++;
+            j--;
+        }
 
-//     while(left<=right){
-//      char temp = s[left];
-//      s[left] = s[right];
-//      s[right] = temp;
-    
-//     left++;
-//     right--;
-    
-//  }
-//}
 
-public void reverseString(char[] s) {
-    int n = s.length;
-    
-    reverse(s , 0 , n);
-}
 
-static void reverse(char[] s , int i , int n){
-    if(i>= n/2){
-        return ;
+        
     }
-
-    char temp = s[i];
-    s[i] = s[n-i-1];
-    s[n-i-1] = temp;
-
-
-    reverse(s, i+1 , n);
-}
 }
