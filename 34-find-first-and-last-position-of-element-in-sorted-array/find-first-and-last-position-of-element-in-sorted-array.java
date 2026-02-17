98af362cb61859ca934782d4f1ad1b9e79d339ef
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-02-17 19:01:19 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (28.64%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,35 +1,53 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
- int[] ans = {-1,-1};
-  ans[0] = search(nums,target,true);
-  if(ans[0]!=-1){
-      ans[1] = search(nums,target,false);
-  }
-  return ans;
-    }
-        int search(int[]nums,int target , boolean  FirstIndex){
-            int start = 0;
-            int end = nums.length-1;
-            int ans = -1;
-            while(start<=end){
-                int mid = start + (end-start)/2;
-                if(nums[mid]>target){
-                    end = mid - 1;
-                }
-                else if(nums[mid]<target){
-                    start = mid + 1;
-                }
-                else{
-                ans = mid;
-                if (FirstIndex){
-                    end = mid - 1;
-                }
-                else{
-                    start = mid + 1;
-                }
-                }
-            }
-        
+
+        int [] ans = {-1,-1};
+
+        ans[0] = search(nums, target , true);
+
+        if(ans[0]!= -1){
+            ans[1] = search(nums, target , false);
+        }
+
+
         return ans;
+        
+        
+    }
+
+
+    public static int search(int[] nums , int target , boolean firstIndex ){
+
+        int left = 0;
+        int right = nums.length -1;
+
+        int res = -1;
+
+        while(left<= right){
+            int mid = left + (right-left)/2;
+
+            if(nums[mid]> target){
+                right = mid - 1;
+            }
+            else if (nums[mid]< target){
+                left = mid +1;
+            }
+
+            else{
+
+                res = mid;
+
+                if(firstIndex){
+                    right = mid -1;
+                }
+
+                else{
+                    left = mid +1;
+                }
+
+            }
+        }
+
+        return res;
     }
 }
