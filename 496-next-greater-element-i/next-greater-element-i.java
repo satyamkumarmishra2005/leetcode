f885a975008af4eb3dd75bc52cde8fff92d92785
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-02-26 11:03:27 +0530

    Time: 4 ms (73.45%) | Memory: 45.4 MB (47.64%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,55 +1,49 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
-        if(nums2.length==0 || nums1.length==0){
+        if(nums2.length==0 || nums1.length==0 ){
             return new int[0];
         }
+    int n = nums2.length;
 
-        Map<Integer, Integer> map = new HashMap<>();
+    Map<Integer, Integer> map = new HashMap<>();
+    Stack<Integer> st = new Stack<>();
 
-        Stack<Integer> st = new Stack<>();
+    st.push(nums2[n-1]); // push the last elemnt in stack
+    map.put(nums2[n-1],-1); // the next greater elemnt of the last elemnt will be -1
 
-        int n = nums2.length;
+    for(int i = n-2 ; i>=0; i--){
+        if(nums2[i]<st.peek()){
 
-        st.push(nums2[n-1]);
-        map.put(nums2[n-1], -1); // Key , Value
-
-        for(int i = n-2 ; i>= 0 ; i--){
-            if(nums2[i]< st.peek()){
-                map.put(nums2[i], st.peek());
-                st.push(nums2[i]);
-
-                continue;
-            }
-
-            while(!st.isEmpty() && nums2[i]> st.peek()){
-                st.pop();
-
-              
-            }
-
-            if(st.isEmpty()){
-                st.push(nums2[i]);
-                map.put(nums2[i],-1);
-            }
-
-            else{
-                map.put(nums2[i], st.peek());
-                st.push(nums2[i]);
-            }
-
-
-
-
-
+            map.put(nums2[i], st.peek());
+            st.push(nums2[i]);
+            
+            continue;
         }
 
-
-        for(int i = 0 ; i< nums1.length ; i++){
-            nums1[i]= map.get(nums1[i]);
+        while(!st.isEmpty() && nums2[i]> st.peek()){
+            st.pop();
         }
 
-        return nums1;
+        if(st.isEmpty()){
+            st.push(nums2[i]);
+            map.put(nums2[i], -1);
+        }
+
+        else{
+            map.put(nums2[i], st.peek());
+            st.push(nums2[i]);
+            
+        }
+    }
+
+
+    for(int i =0 ; i< nums1.length; i++){
+        nums1[i] = map.get(nums1[i]);
+    }
+
+
+return nums1;
         
     }
 }
