3316a21deea51ded89f52bbb6b1fe8b2ad46d1f8
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-03-03 20:16:59 +0530

    Time: 10 ms (86.56%) | Memory: 46.4 MB (74.88%) - LeetSync

--- 224-basic-calculator/basic-calculator.java
+++ 224-basic-calculator/basic-calculator.java
@@ -3,41 +3,46 @@ class Solution {
 
         Stack<Integer> stack = new Stack<>();
 
-        int res = 0;
-        int curr = 0;
-        int sign = 1;
+     int res = 0;
+     int num = 0;
+     int sign = 1;
 
-        for (char c : s.toCharArray()) {
+     for(char c : s.toCharArray() ){
 
-            if (Character.isDigit(c)) {
-                curr = curr * 10 + (c - '0');
-            } 
-            else if (c == '+') {
-                res += curr * sign;
-                sign = 1;
-                curr = 0;
-            } 
-            else if (c == '-') {
-                res += curr * sign;
-                sign = -1;
-                curr = 0;
-            } 
-            else if (c == '(') {
-                stack.push(res);
-                stack.push(sign);
-                res = 0;
-                sign = 1;
-                curr = 0;
-            } 
-            else if (c == ')') {
-                res += curr * sign;
-                curr = 0;
-                res *= stack.pop();
-                res += stack.pop();
-            }
+        if(Character.isDigit(c)){
+            num = num *10 + (c-'0');
         }
 
-        res += sign * curr;
-        return res;
-    }
+        else if(c== '+'){
+            res = res + num* sign;
+            sign= 1;
+            num = 0;
+        }
+
+        else if (c=='-'){
+            res = res + num* sign;
+            sign = -1;
+            num = 0;
+        }
+
+        else if (c=='('){
+            stack.push(res);
+            stack.push(sign);
+
+            res=0;
+            num = 0;
+            sign = 1;
+        }
+
+        else if (c==')'){
+            res = res + num*sign;
+            num=0;
+            res*= stack.pop();
+            res+= stack.pop();
+
+        }
+     }
+       res+= sign*num;
+       return res;
+}
 }
