3ecc28e62c8ec244171063aadc889789d186a5ce
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-03-05 11:20:39 +0530

    Time: 20 ms (65.52%) | Memory: 48.2 MB (49.95%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,47 +1,48 @@
 class Solution {
     public int calculate(String s) {
 
-        Stack<Integer> stack = new Stack<>();
-        int num = 0;
+        Stack<Integer> st = new Stack<>();
+
+        int num =0;
         char sign = '+';
 
-        for(int i = 0; i < s.length(); i++){
-
+        for(int i =0; i< s.length(); i++){
             char c = s.charAt(i);
 
             if(Character.isDigit(c)){
-                num = num * 10 + (c - '0');
+            num = num * 10 + (c-'0');
             }
 
-            if((!Character.isDigit(c) && c != ' ') || i == s.length()-1){
+            if(!Character.isDigit(c) && c!= ' ' || i ==s.length()-1){
 
-                if(sign == '+'){
-                    stack.push(num);
+                if(sign=='+'){
+                    st.push(num);
                 }
 
-                else if(sign == '-'){
-                    stack.push(-num);
+                else if(sign=='-'){
+                    st.push(-num);
                 }
 
-                else if(sign == '*'){
-                    stack.push(stack.pop() * num);
+                else if(sign=='*'){
+                    st.push(st.pop()*num);
                 }
 
-                else if(sign == '/'){
-                    stack.push(stack.pop() / num);
+               else if(sign=='/'){
+                    st.push(st.pop()/ num);
                 }
 
                 sign = c;
-                num = 0;
+                num=0;
             }
         }
 
-        int result = 0;
 
-        for(int n : stack){
+       int result = 0;
+        for(int n : st){
             result += n;
         }
 
         return result;
+        
     }
 }
