197cfaddfd43e3f85c724ad2388de9d0f23e7b65
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-04-01 18:34:16 +0530

    Time: 1 ms (99.97%) | Memory: 50.3 MB (10.21%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -10,40 +10,41 @@
  */
 class Solution {
 
-// Steps ; Reverse the LinkedList from mid to end
-// Proceed with while loop
-    ListNode reverseLinkedList(ListNode head){
-        if(head==null || head.next== null){
-            return head;
-        }
-
-        ListNode last = reverseLinkedList(head.next);
-        head.next.next = head;
-        head.next= null;
-        return last;
-    }
+    ListNode curr;
     public void reorderList(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast!= null && fast.next!= null){
-            slow = slow.next;
-            fast = fast.next.next;
-            
 
+     curr = head;
+
+     solve(head);
+        
+    }
+
+
+    public void solve(ListNode head){
+
+        if(head== null){
+            return ;
         }
 
-        ListNode rev = reverseLinkedList(slow);
-        ListNode curr = head;
+        solve(head.next);
 
-        while(rev.next!= null){
-            ListNode tempcurr = curr.next;
-            curr.next = rev;
-            ListNode temprev = rev.next;
-            
-            rev.next = tempcurr;
+        ListNode temp = curr.next;
 
-            curr = tempcurr;
-            rev = temprev;
+        if(temp== null){
+            return ;
         }
+
+        else if (head == curr){
+            curr.next = null;
+            return ;
+        }
+
+        curr.next = head;
+        head.next = (head==temp) ? null : temp;
+
+        curr = temp;
+
+        
+
     }
 }
