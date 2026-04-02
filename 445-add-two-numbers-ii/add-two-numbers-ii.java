27b7be09d4aec70e35be9d045e3ccc958928f237
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-04-02 14:27:21 +0530

    Time: 4 ms (19.39%) | Memory: 46.7 MB (37.20%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/add-two-numbers-ii.java
@@ -10,36 +10,49 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    Stack<Integer> s1 = new Stack<>();
-    Stack<Integer> s2 = new Stack<>();
 
-    while (l1 != null) {
-        s1.push(l1.val);
-        l1 = l1.next;
+        ListNode head = null;
+
+        Stack<Integer> st1 = new Stack<>();
+
+        Stack<Integer> st2 = new Stack<>();
+
+        while(l1!= null){
+            st1. push(l1.val);
+            l1 = l1.next;
+
+        }
+
+
+        while(l2!= null){
+            st2.push(l2.val);
+            l2 = l2.next;
+        }
+        
+
+        int carry = 0;
+
+        while(!st1.isEmpty() || !st2.isEmpty() || carry!=0 ){
+            int sum = carry;
+
+            if(!st1.isEmpty()){
+                sum = sum + st1.pop();
+            }
+
+            if(!st2.isEmpty()){
+                sum = sum + st2.pop();
+            }
+
+            carry = sum/ 10;
+
+            ListNode node = new ListNode(sum%10);
+
+            node.next = head;
+            head = node;
+
+
+        }
+
+        return head;
     }
-
-    while (l2 != null) {
-        s2.push(l2.val);
-        l2 = l2.next;
-    }
-
-    int carry = 0;
-    ListNode head = null;
-
-    while (!s1.isEmpty() || !s2.isEmpty() || carry != 0) {
-        int sum = carry;
-
-        if (!s1.isEmpty()) sum += s1.pop();
-        if (!s2.isEmpty()) sum += s2.pop();
-
-        ListNode node = new ListNode(sum % 10);
-        node.next = head;
-        head = node;
-
-        carry = sum / 10;
-    }
-
-    return head;
-}
-
 }
