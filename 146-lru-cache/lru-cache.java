debaca9ad690cc5da4b62af9af8012b747342cf2
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-04-09 09:53:56 +0530

    Time: 46 ms (62.44%) | Memory: 130.4 MB (92.64%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,37 +1,29 @@
 class LRUCache {
 
     class Node{
+        int key , value;
+        Node next , prev;
 
-        int value;
-        int key;
-
-        Node next;
-        Node prev;
-
-        Node(int value , int key){
-            this.value = value;
+        Node(int key , int value){
             this.key = key;
+            this.value = value;
         }
     }
 
 
     Node head;
     Node tail;
-
     int capacity;
 
     Map<Integer, Node> map = new HashMap<>();
 
     public LRUCache(int capacity) {
-
         this.capacity = capacity;
-
         head = new Node(0,0);
         tail = new Node(0,0);
 
         head.next = tail;
         tail.prev = head;
-        
     }
     
     public int get(int key) {
@@ -42,8 +34,8 @@ class LRUCache {
 
         Node node = map.get(key);
 
-        remove(node); // remove from previous position
-        insert(node); // insert at front
+        remove(node);
+        insert(node);
 
         return node.value;
         
@@ -52,29 +44,35 @@ class LRUCache {
     public void put(int key, int value) {
 
         if(map.containsKey(key)){
-
             Node node = map.get(key);
+
             node.value = value;
-            remove(node); // remove from current position
-            insert(node); // insert at front
+
+            remove(node);
+            insert(node);
 
             return ;
-
         }
 
-        if(map.size()== capacity){
+        if(map.size()==capacity){
             Node lru = tail.prev;
-            remove(lru); // remove least recently used which is tail.prev last(node)
+
+            remove(lru);
+
             map.remove(lru.key);
         }
 
-        Node newnode = new Node(value, key);
+        Node newnode = new Node(key, value);
 
-        insert(newnode); // insert front
+        insert(newnode);
+
         map.put(key, newnode);
+
+        
     }
 
     public void remove(Node node){
+
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
@@ -82,12 +80,11 @@ class LRUCache {
 
     public void insert(Node node){
         node.next = head.next;
+        
         node.prev = head;
 
         head.next.prev = node;
         head.next = node;
-
-
     }
 }
 
