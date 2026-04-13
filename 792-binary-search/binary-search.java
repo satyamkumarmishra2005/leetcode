579711112d48d39de36208ffe688cb1c3f2dd819
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-04-13 18:28:08 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (94.96%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,29 +1,32 @@
 class Solution {
     public int search(int[] nums, int target) {
 
-        int n = nums.length;
+    return binarysearch(nums, 0 , nums.length-1 , target);
+        
+    }
 
-        int left = 0;
-        int right = n-1;
 
-        while(left<= right){
-            int mid = (left+right)/2;
+    public int binarysearch(int[] nums , int left , int right , int target){
 
-            if(nums[mid]== target){
-                return mid;
-            }
-
-            else if (nums[mid]< target){
-                left = mid +1;
-            }
-
-            else{
-                right = mid-1;
-            }
+        if(left> right){
+            return -1;
         }
 
+        int mid = left + (right-left)/2;
 
-        return -1;
+        if(nums[mid]== target){
+
+            return mid;
+
+
+        }
+
+        if(target< nums[mid]){
+            return binarysearch(nums, left , mid-1, target);
+        }
+
+        
+     return binarysearch(nums, mid+1 , right, target);
         
     }
 }
