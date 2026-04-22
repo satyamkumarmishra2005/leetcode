dd3c03b4011531909dd3172a623ee11c9ef88957
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-04-22 20:35:37 +0530

    Time: 2 ms (37.83%) | Memory: 45.6 MB (49.38%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,32 +1,42 @@
 class Solution {
+
+     Set<Integer> st = new HashSet<>();
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> current = new ArrayList<>();
 
-        boolean[] freq = new boolean[nums.length];
-
-        recurpermutation(result , current , nums , freq);
+        solve(nums , result , new ArrayList<>());
 
         return result;
+    } 
 
 
-    }
+    public void solve(int [] nums , List<List<Integer>> result , List<Integer> temp){
 
-    public void recurpermutation(List<List<Integer>> result ,  List<Integer> current , int[] nums , boolean[] freq  ){
+       
 
-     if(current.size()== nums.length){
-        result.add(new ArrayList<>(current));
-        return;
-     }
-
-     for(int i =0 ; i< nums.length ; i++){
-        if(!freq[i]){
-            freq[i]= true;
-            current.add(nums[i]);
-            recurpermutation(result , current , nums , freq);
-            current.remove(current.size()-1);
-            freq[i] = false;
+        if(temp.size()== nums.length){
+            result.add(new ArrayList<>(temp));
+            return;
         }
-     }
+
+        for(int i = 0 ; i< nums.length ; i++){
+
+            if(!st.contains(nums[i])){
+
+                temp.add(nums[i]);
+                st.add(nums[i]);
+
+                solve(nums , result , temp);
+
+
+               temp.remove(temp.size()-1);
+
+               st.remove(nums[i]);    
+            }
+        }
+
+        
+
+
     }
 }
