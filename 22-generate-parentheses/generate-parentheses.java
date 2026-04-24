eb2df57e6bfe22afd38506933b45310a7f6ff8df
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-04-24 09:57:50 +0530

    Time: 2 ms (71.75%) | Memory: 45 MB (23.69%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,25 +1,35 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> result = new ArrayList<>();
-        backtrack(result, "", 0, 0, n);
-        return result;
+        
+
+        List<String> ans = new ArrayList<>();
+
+
+        backtrack(ans , "", 0,0,n);
+
+
+        return ans;
+
+
     }
 
-    private void backtrack(List<String> result, String current, int open, int close, int max) {
-        // Base case: if the string is complete
-        if (current.length() == max * 2) {
-            result.add(current);
+
+    public void backtrack(List<String> ans , String str , int open , int close , int max){
+
+
+        if(str.length()== max*2){
+            ans.add(str);
+
             return;
         }
 
-        // If we can still add an opening parenthesis
-        if (open < max) {
-            backtrack(result, current + "(", open + 1, close, max);
+
+        if(open< max){
+            backtrack(ans, str+"(", open+1, close , max);
         }
 
-        // If we can add a closing parenthesis (only if close < open)
-        if (close < open) {
-            backtrack(result, current + ")", open, close + 1, max);
+        if(close< open){
+            backtrack(ans , str+")" , open , close+1 , max);
         }
     }
 }
