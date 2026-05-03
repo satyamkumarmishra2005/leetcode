cf4b60d347748bf575b083790012386040aa9309
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-05-03 19:21:45 +0530

    Time: 26 ms (56.20%) | Memory: 53.5 MB (36.35%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.java
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.java
@@ -1,39 +1,45 @@
 class Solution {
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
-
-        Map<Integer, Integer> map = new HashMap<>();
-
-        // Step 1: build row masks
-        for (int[] seat : reservedSeats) {
-            int row = seat[0];
-            int col = seat[1];
-
-            map.put(row, map.getOrDefault(row, 0) | (1 << (col - 1)));
+        Map<Integer,List<Integer>> map = new HashMap<>();
+        
+        
+        for(int[] i : reservedSeats){
+            map.putIfAbsent(i[0],new ArrayList<>());
+            map.get(i[0]).add(i[1]);
         }
-
-        int result = 0;
-
-        // Masks for blocks
-        int leftMask   = 0b0000011110; // seats 2-5
-        int middleMask = 0b0001111000; // seats 4-7
-        int rightMask  = 0b0111100000; // seats 6-9
-
-        // Step 2: process reserved rows
-        for (int mask : map.values()) {
-            boolean leftFree   = (mask & leftMask) == 0;
-            boolean rightFree  = (mask & rightMask) == 0;
-            boolean middleFree = (mask & middleMask) == 0;
-
-            if (leftFree && rightFree) {
-                result += 2;
-            } else if (leftFree || rightFree || middleFree) {
-                result += 1;
+        
+        
+        int ans = 2*(n - map.size());
+        
+        for(Map.Entry<Integer,List<Integer>> entry : map.entrySet()){
+            List<Integer> seats = entry.getValue();
+            
+            boolean isLeftAisle = false, isRightAisle = false, isMiddle = false;
+            
+            for(int seat : seats){
+                
+                if(seat >= 2 && seat <= 5)
+                    isLeftAisle = true;
+                if(seat >= 6 && seat <= 9)
+                    isRightAisle = true;
+                if(seat >=4 && seat <= 7)
+                    isMiddle = true;
+                    
+                if(isLeftAisle && isRightAisle && isMiddle){
+                    break;
+                }
             }
+            
+            if(!isLeftAisle)
+                ans += 1;
+            if(!isRightAisle)
+                ans += 1;
+            if(isLeftAisle && isRightAisle && !isMiddle)
+                ans += 1;
+            
         }
-
-        // Step 3: rows with no reservations
-        result += (n - map.size()) * 2;
-
-        return result;
+       
+        return ans;
+        
     }
 }
