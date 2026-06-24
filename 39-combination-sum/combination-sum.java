2e07134975621c1a5a341a7aeee0c53448205cd4
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-06-24 11:22:52 +0530

    Time: 2 ms (95.64%) | Memory: 45.3 MB (95.85%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,44 +1,36 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-
         List<List<Integer>> ans = new ArrayList<>();
 
-        findcombination(candidates,0 , target , ans , new ArrayList<>());
-
+        findcombination(0, ans , candidates , target , new ArrayList<>());
 
         return ans;
     }
 
 
+    public void findcombination(int indx , List<List<Integer>> ans , int[] candidates, int target , List<Integer> ds){
 
-public void findcombination(int[] arr, int indx , int target , List<List<Integer>>ans , List<Integer> ds ){
+        if(indx== candidates.length){
+
+            if(target==0){
+                ans.add(new ArrayList<>(ds));
+            }
 
 
+            return;
+        }
 
-if(indx== arr.length){
 
-    if(target==0){
-        ans.add(new ArrayList<>(ds));
+        if(candidates[indx]<= target){
 
+            ds.add(candidates[indx]);
+
+            findcombination(indx, ans , candidates , target-candidates[indx], ds);
+
+            ds.remove(ds.size()-1);
+        }
+
+
+        findcombination(indx+1 , ans ,candidates , target, ds);
     }
-
-    return;
-}
-
-
-   if(arr[indx]<= target){
-
-    ds.add(arr[indx]);
-
-    findcombination(arr, indx, target-arr[indx], ans , ds); // since we can repeat an element as many 
-    // times as wanted hence we continue to explore on that index only 
-
-    ds.remove(ds.size()-1);  // remove the last elemnt from datastructure while returning back
-
-   }
-
-
-   findcombination(arr, indx+1 , target, ans , ds); 
-
-}
 }
