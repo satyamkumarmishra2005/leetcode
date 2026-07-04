de8256844ddd3d8b460e8515d46df84034f5a77c
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-07-04 11:14:48 +0530

    Time: 3 ms (90.90%) | Memory: 46.1 MB (99.12%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,48 +1,44 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
 
-        List<List<Integer>> result = new ArrayList<>();
-
-            Arrays.sort(nums);
+        Arrays.sort(nums);
 
         boolean[] visited = new boolean[nums.length];
 
-        solve(nums , result , new ArrayList<>(), visited);
+        findpermutation(nums,visited, ans , new ArrayList<>());
 
-        return result;
+        return ans;
         
     }
 
 
-     public void solve(int [] nums , List<List<Integer>> result , List<Integer> temp , boolean [] visited){
+    public void findpermutation(int[]nums , boolean[] visited , List<List<Integer>> ans, List<Integer> ds ){
 
-       
-
-        if(temp.size()== nums.length){
-            result.add(new ArrayList<>(temp));
+        if(ds.size()==nums.length){
+            ans.add(new ArrayList<>(ds));
             return;
         }
+     
+     for(int i =0 ; i< nums.length; i++){
 
-        for(int i = 0 ; i< nums.length ; i++){
-
-            if(visited[i]) continue;
-
-            if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1]) continue;
-
-                temp.add(nums[i]);
-
-                visited[i] = true;
-
-                solve(nums , result , temp , visited);
-
-
-               temp.remove(temp.size()-1);
-
-               visited[i]= false;    
-            }
+        if(visited[i]){
+            continue;
         }
 
-        
+        if(i>0 && nums[i]== nums[i-1] && !visited[i-1]){
+            continue;
+        }
 
+        ds.add(nums[i]);
+        visited[i]= true;
+
+        findpermutation(nums, visited, ans,ds);
+
+        ds.remove(ds.size()-1);
+
+        visited[i]= false;
+     }
 
     }
+}
