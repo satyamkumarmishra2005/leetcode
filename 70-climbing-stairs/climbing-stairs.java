8332d745e5f3123278d8fbaf87264d6553232701
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-07-15 12:27:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (36.05%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,29 +1,33 @@
 class Solution {
+    // Memoaization solution
     public int climbStairs(int n) {
-        // It will give time limit exced
-//         if(n<0){
-//             return 0;
-//         }
 
-//         if(n == 0){
-//             return 1; // we have got one way to cover all stairs
-//         }
+        int [] dp = new int[n+1];
+        Arrays.fill(dp, -1);
 
-//         int one_step = climbStairs(n-1);
-//         int two_step = climbStairs(n-2);
+        return solve(n, dp);
+        
+    }
 
-//         return one_step + two_step;
-//     }
-// }
 
- int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
+    public int solve(int n , int[] dp){
 
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        if(n<0){
+            return 0;
         }
 
-        return dp[n];        
+        if(n<=1){
+            return 1;
+        }
+
+
+        if(dp[n]!= -1){
+            return dp[n];
+        }
+
+
+        dp[n] = solve(n-1, dp) + solve(n-2,dp);
+
+        return dp[n];
     }
 }
