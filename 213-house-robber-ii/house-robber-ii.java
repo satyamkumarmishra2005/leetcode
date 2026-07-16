8e4ae629444e0cb83b953f5f36fa8cf460f383d8
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-07-16 13:17:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (60.73%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,33 +1,38 @@
 class Solution {
-    public int rob(int[] a) {
 
-        int n = a.length;
-        if(n==0) return 0;
-        if(n ==1) return a[0];
-        ArrayList<Integer> a1=new ArrayList<>();
-        ArrayList<Integer> a2=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(i!=0) a1.add(a[i]);
-            if(i!=n-1) a2.add(a[i]);
+    public int rob(int[] nums) {
+
+        int n = nums.length;
+
+        // Only one house
+        if (n == 1) {
+            return nums[0];
         }
-        return Math.max(solve(a1),solve(a2));
+
+        int case1 = robLinear(nums, 0, n - 2);
+
+        int case2 = robLinear(nums, 1, n - 1);
+
+        return Math.max(case1, case2);
     }
-    public int solve(ArrayList<Integer> a){
-        int n = a.size();
-        int prev1=a.get(0);
-        int prev2=0;
 
-        for(int i=1;i<n;i++){
+    private int robLinear(int[] nums, int start, int end) {
 
-            int pick = a.get(i);
-            if(i>1) pick+=prev2;
-            int nopick = prev1;
+        int prev = 0;
+        int prev2 = 0;
 
-            int cur = Math.max(pick,nopick);
+        for (int i = start; i <= end; i++) {
 
-            prev2=prev1;
-            prev1=cur;
+            int pick = nums[i] + prev2;
+
+            int notPick = prev;
+
+            int curr = Math.max(pick, notPick);
+
+            prev2 = prev;
+            prev = curr;
         }
-        return prev1;        
+
+        return prev;
     }
 }
