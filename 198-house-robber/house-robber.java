0582611d98b8ad024e4b7bebcba77fb720589e4b
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-07-16 12:24:08 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (5.34%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,79 +1,36 @@
 class Solution {
+    public int rob(int[] nums) {
 
-  // Memoaization
-//     public int rob(int[] nums) {
-//         int n = nums.length;
-//         int dp [] = new int [n];
+        int n = nums.length;
 
-//         Arrays.fill(dp , -1);
-//         return solve(n-1 , nums , dp);
+        int[] dp = new int[n];
+
+        Arrays.fill(dp,-1);
+
+
+        return solve(n-1, nums, dp);
         
-//     }
+    }
 
-//     static int solve(int index , int [] nums , int [] dp){
+    public int solve(int indx , int[]nums , int[]dp){
 
-//         if(index < 0){
-//             return 0;
-//         }
-//         if(index == 0){
-//             return nums[index];
-//         }
+        if(indx<0){
+            return 0;
+        }
 
-//         if(dp[index]!= -1){
-//             return dp[index];
-//         }
+        if(indx==0){
+            return nums[indx];
+        }
 
-//         int pick = nums[index] + solve(index-2 , nums , dp);
-//         int nonpick = solve(index-1 , nums , dp);
+        if(dp[indx]!= -1){
+            return dp[indx];
+        }
 
-//         return dp[index] = Math.max(pick , nonpick);
-//     }
-// }
+        int pick = nums[indx] + solve(indx-2, nums , dp);
+
+        int notpick = solve(indx-1, nums, dp);
 
 
-// Tabulation
-// Time complexity = O(N)
-// spcae complexity =O(N)
-public int rob(int[] nums) {
-//     int n = nums.length;
-//     if (n == 0) return 0;
-//     if (n == 1) return nums[0];
-
-//     int[] dp = new int[n];
-//     dp[0] = nums[0];
-//     dp[1] = Math.max(nums[0], nums[1]);
-
-//     for (int i = 2; i < n; i++) {
-//         int pick = nums[i] + dp[i - 2];
-//         int notPick = dp[i - 1];
-//         dp[i] = Math.max(pick, notPick);
-//     }
-
-//     return dp[n - 1];
-// }
-
-
-// space optimization
-int n = nums.length;
-int prev = nums[0];
-int prev2 = 0;
-
-for(int i =1 ; i<n ; i++ ){
-    
-    int pick = nums[i];
-
-    if(i>1)
-
-    pick+= prev2;
-
-    int nonpick = prev;
-
-    int curr_i = Math.max(pick , nonpick);
-    prev2 = prev;
-    prev = curr_i;
-}
-
-return prev;
-
-}
+        return dp[indx] = Math.max(pick, notpick);
+    }
 }
