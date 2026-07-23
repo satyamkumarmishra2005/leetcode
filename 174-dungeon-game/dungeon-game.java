55ac51bd9a80f1eb4347980cf6ba3aaf38b1e0d3
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-07-23 08:46:20 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (56.11%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -6,32 +6,46 @@ class Solution {
         int n = dungeon[0].length;
 
 
-        int [][] dp = new int[m][n];
+        int[][] dp = new int[m][n];
 
-        for(int[] rows: dp){
-            Arrays.fill(rows, -1);
+
+        for(int[] arr: dp){
+            Arrays.fill(arr,-1);
         }
 
 
-        return solve(dungeon , dp , 0, 0, m , n);
+
+        return solve(dungeon, dp, 0,0, m , n);
         
     }
 
 
-   public int solve( int[][] dungeon , int[][]t, int i, int j, int m , int n) {
-        if (i >= m || j >= n)
-            return (int)1e9;
+    public int solve(int[][]dungeon, int[][]dp , int i , int j , int m , int n){
 
-        if (t[i][j] != -1)
-            return t[i][j];
 
-        if (i == m - 1 && j == n - 1)
-            return t[i][j] = (dungeon[i][j] > 0 ? 1 : Math.abs(dungeon[i][j]) + 1);
+        if(i>= m || j>= n){
 
-        int down  = solve( dungeon, t, i + 1, j,m,n);
-        int right = solve( dungeon, t, i, j + 1,m,n);
+          return (int)1e9;
+            
+        }
 
-        int res = Math.min(down, right) - dungeon[i][j];
-        return t[i][j] = (res > 0 ? res : 1);
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+
+        if(i==m-1 && j==n-1){
+            return dp[i][j] = (dungeon[i][j]>0 ? 1: Math.abs(dungeon[i][j])+1);
+        }
+
+
+        int down = solve(dungeon, dp, i+1, j , m , n);
+
+        int right = solve(dungeon, dp, i, j+1, m,n);
+
+
+        int result = Math.min(down,right)- dungeon[i][j];
+
+        return dp[i][j] =( result>0? result: 1);
     }
 }
