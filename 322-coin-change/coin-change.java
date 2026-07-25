f6f3ba9e3a00caafa79046df8952c93b9cb193c0
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-07-25 12:19:18 +0530

    Time: 97 ms (5.01%) | Memory: 47.9 MB (5.55%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,76 +1,49 @@
-import java.util.Arrays;
-
 class Solution {
-    // Memoaization
-//     static int minimumcoinsreq(int[] coins, int indx, int[][] dp, int amount) {
-//         // Base cases
-//         if (amount == 0) return 0; // No coins needed if amount is 0
-//         if (indx < 0 || amount < 0) return (int) Math.pow(10, 9); // Unreachable case
+    public int coinChange(int[] coins, int amount) {
 
-//         // Memoization check
-//         if (dp[indx][amount] != -1) return dp[indx][amount];
+        int n= coins.length;
 
-//         // Not taken: Skip the current coin
-//         int nottaken = minimumcoinsreq(coins, indx - 1, dp, amount);
+        int[][] dp = new int[n][amount+1];
 
-//         // Taken: Include the current coin if possible
-//         int taken = (int) Math.pow(10, 9);
-//         if (coins[indx] <= amount) {
-//             taken = 1 + minimumcoinsreq(coins, indx, dp, amount - coins[indx]);
-//         }
-
-//         // Store the result in the DP table
-//         return dp[indx][amount] = Math.min(nottaken, taken);
-//     }
-
-//     public int coinChange(int[] coins, int amount) {
-//         int n = coins.length;
-//         int[][] dp = new int[n][amount + 1];
-
-//         // Initialize DP table with -1
-//         for (int[] row : dp) {
-//             Arrays.fill(row, -1);
-//         }
-
-//         int ans = minimumcoinsreq(coins, n - 1, dp, amount);
-
-//         // If no solution found, return -1
-//         return ans >= (int) Math.pow(10, 9) ? -1 : ans;
-//     }
-// }
-// Tabulation 
-   public int coinChange(int[] coins, int amount) {
-    int n = coins.length;
-    int[][] dp = new int[n][amount+1];
-
-    for(int i =0 ; i<= amount ; i++){
-       if(i% coins[0]==0){
-        dp[0][i] = i/ coins[0];
-       }
-       else{
-        dp[0][i] = (int) Math.pow(10, 9);
-       }
-    }
-
-    for(int indx = 1; indx < n ; indx++){
-        for(int target = 0 ; target<= amount ; target++){
-          int nottaken = dp[indx-1][target];
-           int take = (int) Math.pow(10, 9);
-
-                // If the current element is less than or equal to the target, calculate 'take'
-                if (coins[indx] <= target)
-                    take = 1 + dp[indx][target - coins[indx]];
-
-                // Store the minimum result in the dp array
-                dp[indx][target] = Math.min(nottaken, take);
+        for(int[]row: dp){
+            Arrays.fill(row,-1);
         }
+
+        int ans = solve(coins, amount , n-1, dp,  n);
+
+     // if answer is not found return -1;
+        return ans >= (int) Math.pow(10, 9)?-1:ans;
+        
     }
 
-      int ans = dp[n - 1][amount];
 
-        // If it's not possible to achieve the target sum, return -1
-        if (ans >= (int) Math.pow(10, 9))
-            return -1;
-        return ans;
+    public int solve(int[]coins , int amount , int indx , int[][]dp, int n){
+
+        if(amount==0){
+            return 0;
+        }
+
+        if(indx<0 || amount<0){
+            return (int) Math.pow(10, 9);
+        }
+
+
+        if(dp[indx][amount]!=-1){
+            return dp[indx][amount];
+        }
+
+        int nottake = solve(coins, amount , indx-1, dp, n);
+
+             
+             
+int take = (int) Math.pow(10, 9);
+
+  if(coins[indx]<=amount){
+   take= 1+solve(coins,amount-coins[indx],indx,dp,n);
+     }
+
+
+   return dp[indx][amount] = Math.min(take, nottake);
+    
     }
-   }
+}
