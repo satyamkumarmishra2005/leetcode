55e93bd71103dff08617f16f2f096b737f2e484e
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-07-26 09:51:10 +0530

    Time: 16 ms (35.88%) | Memory: 57.1 MB (28.94%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,72 +1,55 @@
 class Solution {
+    public int change(int amount, int[] coins) {
 
-    // Memoaization
-    // public int change(int amount, int[] coins) {
-    //     int n = coins.length;
+     int n = coins.length;
 
-    //     int[][] dp = new int[n][amount+1];
-
-    //     for(int [] row : dp){
-    //         Arrays.fill(row , -1);
-    //     }
-    //     return numberofways(n-1 , amount , coins , dp);
-    // }
-
-    // static int numberofways(int index , int amount , int [] coins , int[][] dp){
-    //     if(index ==0){
-    //         if(amount % coins[0] == 0){
-    //             return 1;
-    //         }
-
-    //         else{
-    //             return 0;
-    //         }
-    //     }
+     int[][] dp = new int[n][amount+1];
 
 
-    //     if(dp[index][amount]!=-1){
-    //         return dp[index][amount];
-    //     }
-    //     int nottaken = numberofways(index-1 , amount , coins , dp);
-      
-    //     int taken = 0;
-
-    //     if(coins[index]<= amount){
-    //         taken = numberofways(index , amount-coins[index], coins , dp);
-    //     }
-
-    //     return dp[index][amount] = nottaken + taken ;
-    // }
+     for(int[]row: dp){
+        Arrays.fill(row,-1);
+     }
 
 
-    // Tabulation
-    public int change(int amount, int[] coins){
-        int n = coins.length;
-
-        int[][] dp = new int[n][amount+1];
-
-        for(int i = 0 ; i <= amount ; i++){
-            if(i % coins[0]==0){
-                dp[0][i] = 1;
-            }
-        }
-
-           for(int index = 1 ; index < n ; index++){
-            for(int target =0 ; target<= amount ; target++){
-
-                int nottaken = dp[index-1][target];
-
-                int taken = 0;
-                if(coins[index]<= target){
-                    taken = dp[index][target-coins[index]];
-                }
-
-                dp[index][target] = nottaken + taken ;
-            }
-           
-        }
-
-        return dp[n-1][amount];
+     return solve(amount, coins, dp , n-1);
+        
     }
 
+
+    public int solve(int amount , int[]coins , int[][]dp , int indx){
+
+        if(amount<0){
+            return 0;
+        }
+
+        if(amount==0){
+            return 1;
+        }
+
+        if(indx==0){
+
+            if(amount%coins[0]==0){
+                return 1;
+            }
+
+            return 0;
+        }
+
+       
+
+
+        if(dp[indx][amount]!=-1){
+            return dp[indx][amount];
+        }
+
+        int notpick = solve(amount, coins, dp , indx-1);
+
+        int pick =0;
+
+        if(coins[indx]<=amount){
+            pick =solve(amount-coins[indx], coins, dp , indx);
+        }
+
+        return dp[indx][amount] = pick + notpick;
+    }
 }
