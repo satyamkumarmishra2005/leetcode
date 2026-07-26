7ddb03bdb5b8d0d795b48aa76d19844e42e976eb
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-07-26 09:16:03 +0530

    Time: 4 ms (87.66%) | Memory: 44.8 MB (52.40%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,35 +1,61 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
 
-     return countways(nums, 0 , 0 , target);
+        int n = nums.length;
+        int totalsum =0;
 
-        
-    }
+        for(int num: nums){
+         totalsum = totalsum + num;
+        }
 
-
-    public int countways(int[]nums , int indx , int currsum , int target){
-
-        if(indx==nums.length){
-
-            if(currsum==target){
-                return 1;
-            }
-
+        if ((totalsum - target) < 0 || (totalsum - target) % 2 != 0)
             return 0;
 
-            
+
+        int subsetsum = (totalsum- target)/2;
+
+         int[][] dp = new int[n][subsetsum+1];
+
+         for(int[]row: dp){
+            Arrays.fill(row,-1);
         }
 
 
 
-        int plus = countways(nums, indx+1, currsum+nums[indx], target);
-
-        int minus = countways(nums, indx+1 , currsum-nums[indx], target);
-
-
-
-        return plus + minus;
 
         
+    return solve(nums,subsetsum,n-1, dp);
+    }
+
+    public int solve(int[]nums, int target, int indx, int[][]dp){
+
+        if(indx==0){
+
+            if(target==0 && nums[0]==0){
+                return 2;
+            }
+
+            if(target==0 || target==nums[0]){
+                return 1;
+            }
+
+            return 0;
+        }
+
+
+        if(dp[indx][target]!=-1){
+            return dp[indx][target];
+        }
+
+        int notpick = solve(nums, target, indx-1,dp);
+
+        int pick =0;
+
+        if(nums[indx]<= target){
+            pick = solve(nums, target-nums[indx], indx-1,dp);
+        }
+
+
+        return dp[indx][target] = pick+ notpick;
     }
 }
