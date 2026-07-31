9f11d6e2c3a1c316156198d0ffa92ffa3191632a
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-07-31 10:47:13 +0530

    Time: 17 ms (81.57%) | Memory: 54.6 MB (24.67%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,85 +1,42 @@
 class Solution {
-
-    // Memoaization
-
-    // static int countuntil(String s , String t , int index1 , int index2 , int[][] dp){
-    //    if(index2 < 0){
-    //         return 1;
-    //     }
-       
-    //     if(index1<0){
-    //         return 0;
-    //     }
-        
-
-    //     if(dp[index1][index2]!=-1){
-    //         return dp[index1][index2];
-
-    //     }
-
-    //     if(s.charAt(index1)== t.charAt(index2)){
-    //         return dp[index1][index2] =    countuntil(s,t,index1-1 , index2-1 , dp) + countuntil(s,t,index1-1 , index2 , dp);
-    //     }
-
-    //     else{
-    //         return dp[index1][index2] = countuntil(s,t,index1-1 , index2 , dp);
-    //     }
-        
-    // }
-    // public int numDistinct(String s, String t) {
-    //     int n = s.length();
-    //     int m = t.length();
-
-    //     int [][] dp = new int [n][m];
-
-    //     for(int [] rows : dp){
-    //         Arrays.fill(rows,-1);
-    //     }
-    
-    // return countuntil(s, t, n-1 , m-1 , dp);
-
-
-    // }
-    // Tabulation
-
-     static int countuntil(String s , String t , int index1 , int index2 , int[][] dp){
-        // converted to 1 based indexing
-       if(index2 == 0){
-            return 1;
-        }
-       
-        if(index1==0){
-            return 0;
-        }
-        
-
-        if(dp[index1][index2]!=-1){
-            return dp[index1][index2];
-
-        }
-
-        if(s.charAt(index1-1)== t.charAt(index2-1)){
-            return dp[index1][index2] =    countuntil(s,t,index1-1 , index2-1 , dp) + countuntil(s,t,index1-1 , index2 , dp);
-        }
-
-        else{
-            return dp[index1][index2] = countuntil(s,t,index1-1 , index2 , dp);
-        }
-        
-    }
     public int numDistinct(String s, String t) {
+
         int n = s.length();
+        
         int m = t.length();
 
-        int [][] dp = new int [n+1][m+1];
+        int[][] dp = new int[n][m];
 
-        for(int [] rows : dp){
-            Arrays.fill(rows,-1);
+        for(int[]row:dp){
+            Arrays.fill(row,-1);
         }
-    
-    return countuntil(s, t, n , m , dp);
 
 
+
+        return solve(s,t,n-1,m-1,dp);
+
+
+        
     }
-    
- }
+
+    public int solve(String s, String t, int indx1, int indx2, int[][]dp){
+
+        if(indx2<0){
+            return 1;
+        }
+
+        if(indx1<0){
+            return 0;
+        }
+
+        if(dp[indx1][indx2]!=-1){
+            return dp[indx1][indx2];
+        }
+
+        if(s.charAt(indx1)== t.charAt(indx2)){
+            return dp[indx1][indx2] = solve(s,t,indx1-1,indx2-1,dp) + solve(s,t,indx1-1, indx2, dp);
+        }
+
+        return dp[indx1][indx2] = solve(s,t,indx1-1,indx2,dp);
+    }
+}
