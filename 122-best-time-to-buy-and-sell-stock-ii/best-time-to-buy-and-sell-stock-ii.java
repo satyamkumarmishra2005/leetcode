f78eec0c65c9aacac279387d8ff4ea0fe3af3424
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-08-01 10:29:02 +0530

    Time: 4 ms (10.82%) | Memory: 50.5 MB (5.34%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,84 +1,50 @@
 class Solution {
-   // public int maxProfit(int[] prices) {
-        //here we are allowed to do multiple transactions
-        //each day we have option yo buy or sell
-        //we cant buy if we have alredy buyed a stock and havent sold it
-//         int[][] dp=new int[prices.length+1][2];
-//         for(int[] c:dp)
-//         {
-//             Arrays.fill(c,-1);
-//         }
-//         return solve(0,1,prices,dp);
-//         //expecting the function to return the max profit that it can get while starting on index 0 and we can buy
-//     }
-//     int solve(int ind,int buy,int[] arr,int[][] dp)
-//     {
-//         if(ind==arr.length)
-//         return 0;
-//         if(dp[ind][buy]!=-1)
-//         return dp[ind][buy];
-//         int profit=0;
-//         if(buy==1)
-//         {
-//             //two options buy or not buy
-//             //buy
-//             int op1=-arr[ind]+solve(ind+1,0,arr,dp);
-//             //dont buy
-//             int op2=solve(ind+1,1,arr,dp);
-//             profit=Math.max(op1,op2);
-//         }
-//         else
-//         {
-//             //sell or don't
-//             int op1=arr[ind]+solve(ind+1,1,arr,dp);
-//             int op2=solve(ind+1,0,arr,dp);// dont sell
-//             profit=Math.max(op1,op2);
-//         }
-//         dp[ind][buy]= profit;
-//         return dp[ind][buy];
-//     }
- public int maxProfit(int[] prices) {  // if 
-    int n = prices.length ;
-    int [][] dp = new int[n][2];
+    public int maxProfit(int[] prices) {
 
-    for(int []rows: dp){
-        Arrays.fill(rows,-1);
+        int n = prices.length;
+
+        int[][] dp = new int[n][2];
+
+        for(int[]row: dp){
+            Arrays.fill(row,-1);
+        }
+
+        return solve(prices, 0, dp, 1, n);
+        
     }
 
-    return solve(prices,0 , 1 , dp , n);
- }
+    public int solve(int[]prices, int indx , int[][]dp, int buy, int n){
 
- static int solve(int [] prices , int index, int buy , int [][] dp , int n ){
-    if(index== n){
-        return 0;
+        if(indx==n){
+            return 0;
+        }
+
+        if(dp[indx][buy]!=-1){
+            return dp[indx][buy];
+
+        }
+
+        int profit;
+
+        if(buy==1){
+
+            profit = Math.max( 0 + solve(prices, indx+1,dp,1,n) // skip buy
+            , -prices[indx] + solve(prices,indx+1,dp,0,n)); // buy
+
+        }
+
+        else{
+
+            profit = Math.max(0+ solve(prices, indx+1, dp ,0, n), // skip sell
+            
+            prices[indx]+ solve(prices, indx+1, dp ,1,n)); // sell 
+        }
+
+
+        dp[indx][buy] = profit;
+
+
+        return profit;
+
     }
-
-    int profit = 0;
-
-    if(dp[index][buy]!= -1){
-        return dp[index][buy];
-    }
-
-    if(buy == 1){
-        // we have two options either we can buy or skip
-        // buy
-        int op1 = -prices[index] + solve(prices , index+1 , 0 , dp , n);
-        // skip this move for next
-        int op2 = solve(prices , index+1 , 1, dp , n);
-        profit = Math.max(op1,op2);
-    }
-
-    else{  // sell
-     int op1 = prices[index] + solve(prices , index+1 , 1 , dp , n);
-    
-    // skip this
-     int op2 = solve(prices , index+1 , 0 , dp , n);
-
-     profit = Math.max(op1 , op2);
-         
- }
-
- dp[index][buy] = profit;
- return dp[index][buy];
-}
 }
