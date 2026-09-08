b7b5a7ddf53cd22151624b513ee2007683335563
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-09-08 17:47:39 +0530

    Time: 1 ms (99.93%) | Memory: 77.2 MB (66.29%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,48 +1,13 @@
 class Solution {
-
     public int maxSubArray(int[] nums) {
-        return divide(nums, 0, nums.length - 1);
-    }
+        int maxsum = nums[0];
+        int currentsum = nums[0];
 
-    private int divide(int[] nums, int left, int right) {
 
-        // base case
-        if (left == right) {
-            return nums[left];
+        for(int i=1 ; i<nums.length ; i++){
+            currentsum = Math.max(nums[i], currentsum+nums[i]);
+            maxsum = Math.max(maxsum, currentsum);
         }
-
-        int mid = (left + right) / 2;
-
-        // solve left and right
-        int leftMax = divide(nums, left, mid);
-        int rightMax = divide(nums, mid + 1, right);
-
-        // solve crossing
-        int crossMax = maxCrossing(nums, left, mid, right);
-
-        return Math.max(Math.max(leftMax, rightMax), crossMax);
-    }
-
-    private int maxCrossing(int[] nums, int left, int mid, int right) {
-
-        int sum = 0;
-        int leftSum = Integer.MIN_VALUE;
-
-        // go left from mid
-        for (int i = mid; i >= left; i--) {
-            sum += nums[i];
-            leftSum = Math.max(leftSum, sum);
-        }
-
-        sum = 0;
-        int rightSum = Integer.MIN_VALUE;
-
-        // go right from mid+1
-        for (int i = mid + 1; i <= right; i++) {
-            sum += nums[i];
-            rightSum = Math.max(rightSum, sum);
-        }
-
-        return leftSum + rightSum;
+        return maxsum;
     }
 }
