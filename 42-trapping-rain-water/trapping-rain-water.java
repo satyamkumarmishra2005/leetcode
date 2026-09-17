7ddfb5a75619e534493be8a8a1902a385635a0de
Satyam Mishra <satyamkumarmishra2005@gmail.com>
2026-09-17 10:31:01 +0530

    Time: 1 ms (56.06%) | Memory: 48.9 MB (8.77%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,46 +1,65 @@
 class Solution {
     public int trap(int[] height) {
 
-        int n = height.length;
+        int n =height.length;
 
         if(n==0 || n==1){
             return 0;
         }
 
-        int [] leftmax = getleftmax(height,n);
-        int [] rightmax = getrightmax(height,n);
-        int ans = 0;
+        int[]leftmax = getleftmax(height,n);
+        int[]rightmax= getrightmax(height,n);
 
-        for(int i = 0 ; i<n ; i++){
 
-        ans+= Math.min(leftmax[i], rightmax[i])- height[i];
+        int sum =0;
+
+        for(int i=0; i<n ; i++){
+            sum = sum + Math.min(leftmax[i],rightmax[i])-height[i];
         }
-        
-        return ans;
+       
+
+       return sum;
+
         
     }
 
-    static int [] getleftmax(int[] height , int n){
-        int[] leftmax = new int[n];
-     leftmax[0] = height[0];
+    public static int[] getleftmax(int[]height, int n){
 
-     for(int i =1 ; i< n ; i++){
-        leftmax[i] = Math.max(leftmax[i-1], height[i]);
-     }
+        int[]leftmax =new int[n];
 
-     return leftmax;
+        leftmax[0]= height[0];
+
+
+        for(int i=1 ; i<n ; i++){
+            leftmax[i]= Math.max(leftmax[i-1],height[i]);
+        }
+
+
+        return leftmax;
+
 
     }
 
 
-    static int [] getrightmax(int[] height , int n){
-        int[] rightmax = new int[n];
-        rightmax[n-1] = height[n-1];
 
-        for(int i = n-2 ; i>= 0 ; i--){
-            rightmax[i] = Math.max(rightmax[i+1], height[i]);
-        } 
+    public static int[] getrightmax(int[]height, int n){
+
+        int[]rightmax =new int[n];
+
+        rightmax[n-1]= height[n-1];
+
+
+        for(int i=n-2 ; i>=0 ; i--){
+            rightmax[i]= Math.max(rightmax[i+1],height[i]);
+        }
+
 
         return rightmax;
+
     }
+
+
+
+
+
 }
